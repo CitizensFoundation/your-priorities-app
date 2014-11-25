2b72f389c0207d2fe71ba44d32360e082b616199
Róbert Viðar Bjarnason <robert@citizens.is>
2014-11-25 11:16:22 +0000

    Moved things around

--- server_api/api_app.js
+++ server_api/api_app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client_app')));
+app.use(express.static(path.join(__dirname, '../client_app')));
 
 app.use('/', routes);
 app.use('/api/ideas', ideas);
