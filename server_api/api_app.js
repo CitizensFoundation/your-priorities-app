b09af88307a5437036fa8b00a7b24c891f350f52
Róbert Viðar Bjarnason <robert@citizens.is>
2014-12-06 14:37:24 +0000

    Server app from app folder not dist folder in dev

--- server_api/api_app.js
+++ server_api/api_app.js
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client_dist')));
+app.use(express.static(path.join(__dirname, '../client_app')));
 
 
 app.use('/', routes);
