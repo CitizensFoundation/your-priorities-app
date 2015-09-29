fb6fdd6f20c7e9f791ff0c625ad28fc1c5ac4aab
Róbert Viðar Bjarnason <robert@citizens.is>
2015-09-29 15:49:47 +0000

    Various

---
+++ server_api/controllers/categories.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var models = require("../models");
+
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.send(401, 'Unauthorized');
+};
+
+/* GET ideas listing. */
+router.get('/', function(req, res) {
+  models.Category.findAll({
+      include: [ models.Image ]
+  }).then(function(categories) {
+    res.send(categories);
+  });
+});
+
+router.get('/:id', function(req, res) {
+  models.Category.find({
+    where: { id: req.params.id },
+    include: [
+      { model: models.Group,
+        order: 'Group.created_at DESC'
+      },
+      {
+        model: models.Image, as: 'CategoryIconImages'
+      },
+      {
+        model: models.Image, as: 'CategoryHeaderImages'
+      }
+    ]
+  }).then(function(category) {
+    res.send(category);
+  });
+});
+
+router.post('/:groupId', isAuthenticated, function(req, res) {
+  var category = models.Category.build({
+    name: req.body.name,
+    description: req.body.description,
+    user_id: req.user.id,
+    group_id: req.params.groupId
+  });
+  category.save().then(function() {
+    category.setupImages(req.body, function(err) {
+      if (err) {
+        res.sendStatus(403);
+        console.error(err);
+      } else {
+        res.send(category);
+      }
+    });
+  });
+});
+
+router.put('/:id', isAuthenticated, function(req, res) {
+  models.Category.find({
+    where: { id: req.params.id }
+  }).then(function(category) {
+    category.name = req.body.name;
+    category.description = req.body.description;
+    category.save().then(function () {
+      category.setupImages(req.body, function(err) {
+        if (err) {
+          res.sendStatus(403);
+          console.error(err);
+        } else {
+          res.send(category);
+        }
+      });
+    });
+  });
+});
+
+module.exports = router;
--- server_api/models/category.js
+++ server_api/models/category.js
@@ -7,7 +7,55 @@ module.exports = function(sequelize, DataTypes) {
     icon_file_name: DataTypes.STRING
   }, {
     underscored: true,
+    
     tableName: 'categories',
+
+    instanceMethods: {
+
+      setupIconImage: function(body, done) {
+        if (body.uploadedIconImageId) {
+          sequelize.models.Image.find({
+            where: {id: body.uploadedIconImageId}
+          }).then(function (image) {
+            if (image)
+              this.addCategoryIconImage(image);
+            done();
+          }.bind(this));
+        } else done();
+      },
+
+      setupHeaderImage: function(body, done) {
+        if (body.uploadedHeaderImageId) {
+          sequelize.models.Image.find({
+            where: {id: body.uploadedHeaderImageId}
+          }).then(function (image) {
+            if (image)
+              this.addCategoryHeaderImage(image);
+            done();
+          }.bind(this));
+        } else done();
+      },
+
+      setupImages: function(body, done) {
+        async.parallel([
+          function(callback) {
+            this.setupIconImage(body, function (err) {
+              if (err) return callback(err);
+              callback();
+            });
+          }.bind(this),
+          function(callback) {
+            this.setupHeaderImage(body, function (err) {
+              if (err) return callback(err);
+              callback();
+            });
+          }.bind(this)
+        ], function(err) {
+          done(err);
+        });
+      }
+    },
+    
     classMethods: {
       associate: function(models) {
         Category.hasMany(models.Idea);
