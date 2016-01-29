298c7811b8dfb536b3acfc55735dc8bfe275072c
Róbert Viðar Bjarnason <robert@citizens.is>
2016-01-29 12:02:53 +0000

    Fixes for notifications

--- server_api/active_citizen/workers/notification.js
+++ server_api/active_citizen/workers/notification.js
@@ -1,5 +1,6 @@
 // https://gist.github.com/mojodna/1251812
 
+var async = require("async");
 var NotificationWorker = function () {};
 var models = require("../../models");
 var log = require('../utils/logger');
@@ -9,26 +10,50 @@ var toJson = require('../utils/to_json');
 
 NotificationWorker.prototype.process = function (notification, done) {
 
-  var user = notification.activity.actor.user;
+  var user;
   var domain = notification.activity.object.domain;
   var community = notification.activity.object.community;
 
-  user.setLocale(i18n, domain, community, function () {
-    log.info('Processing Notification Started', { type: notification.type, user: user });
-
-    switch(notification.type) {
-      case models.AcNotification.NOTIFICATION_PASSWORD_RECOVERY:
-        queue.create('send-one-email', {
-          subject: i18n.t('email.password_recovery'),
-          template: 'password_recovery',
-          user: user,
-          domain: domain,
-          community: community,
-          token: notification.activity.object.token
-        }).priority('critical').removeOnComplete(true).save();
-        log.info('Processing Notification Completed', { type: notification.type, user: user });
-        done();
-        break;
+  async.series([
+    function(callback){
+      models.User.find({
+        where: { id: notification.activity.actor.user.id }
+      }).then(function(userResults) {
+        if (userResults) {
+          user = userResults;
+          callback();
+        } else {
+          callback('User not found');
+        }
+      }).catch(function(error) {
+        callback(error);
+      });
+    },
+    function(callback){
+      user.setLocale(i18n, domain, community, function () {
+        callback();
+      });
+    }
+  ],
+  function(error) {
+    if (error) {
+      log.error("NotificationWorker Error", {err: error});
+    } else {
+      log.info('Processing Notification Started', { type: notification.type, user: user });
+      switch(notification.type) {
+        case models.AcNotification.NOTIFICATION_PASSWORD_RECOVERY:
+          queue.create('send-one-email', {
+            subject: i18n.t('email.password_recovery'),
+            template: 'password_recovery',
+            user: user,
+            domain: domain,
+            community: community,
+            token: notification.activity.object.token
+          }).priority('critical').removeOnComplete(true).save();
+          log.info('Processing Notification Completed', { type: notification.type, user: user });
+          done();
+          break;
+      }
     }
   });
 };
