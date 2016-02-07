88d5e48371604d1c7f5fd336452a9494cdcb8a9f
Róbert Viðar Bjarnason <robert@citizens.is>
2016-02-07 23:27:28 +0000

    Fix for iron pages not working on chrome on ipad

--- client_app/scripts/appAfter.js
+++ client_app/scripts/appAfter.js
@@ -6,12 +6,6 @@
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
-  });
-
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
