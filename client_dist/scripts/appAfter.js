79139688e4a668a2d1c1a4287770e2e3d3fcd529
Róbert Viðar Bjarnason <robert@citizens.is>
2016-02-26 18:42:00 +0000

    New dist

--- client_dist/scripts/appAfter.js
+++
@@ -1,1 +0,0 @@
-!function(e){"use strict";var n=e.querySelector("#app");window.addEventListener("WebComponentsReady",function(){}),n.scrollPageToTop=function(){e.getElementById("mainArea")}}(document);
