daf88c37806328eb3f86cdfb9156695f098e4a9c
Róbert Viðar Bjarnason <robert@citizens.is>
2016-03-07 15:56:13 +0000

    Various

--- server_api/utils/logger.js
+++ server_api/utils/logger.js
@@ -1,4 +1,23 @@
 var bunyan = require('bunyan');
-var logger = bunyan.createLogger({name: "your-priorities"});
+var PrettyStream = require('bunyan-prettystream');
+
+var logger;
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV != 'production') {
+  var prettyStdOut = new PrettyStream();
+  prettyStdOut.pipe(process.stdout);
+
+  logger = bunyan.createLogger({
+    name: 'foo',
+    streams: [{
+      level: 'debug',
+      type: 'raw',
+      stream: prettyStdOut
+    }]
+  });
+} else {
+  logger = bunyan.createLogger({name: "your-priorities"});
+}
 
 module.exports = logger;
