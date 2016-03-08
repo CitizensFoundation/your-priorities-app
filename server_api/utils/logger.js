949eefe7038450faf46246914fbdd45fbf11dbd4
Róbert Viðar Bjarnason <robert@citizens.is>
2016-03-08 14:24:13 +0000

    Work on notifications

--- server_api/utils/logger.js
+++ server_api/utils/logger.js
@@ -2,14 +2,13 @@ var bunyan = require('bunyan');
 var PrettyStream = require('bunyan-prettystream');
 
 var logger;
-console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV != 'production') {
   var prettyStdOut = new PrettyStream();
   prettyStdOut.pipe(process.stdout);
 
   logger = bunyan.createLogger({
-    name: 'foo',
+    name: 'your-priorities',
     streams: [{
       level: 'debug',
       type: 'raw',
