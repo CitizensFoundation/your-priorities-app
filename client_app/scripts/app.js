f2242769e05af13cc8d3864f44c34005b2042f78
Róbert Viðar Bjarnason <robert@citizens.is>
2016-03-28 13:19:29 +0000

    Splash screens

--- client_app/scripts/app.js
+++ client_app/scripts/app.js
@@ -1,5 +1,34 @@
+var splashDiv;
+
+if (window.location.hostname.indexOf('betrireykjavik') > -1) {
+  splashDiv = document.createElement("div");
+  splashDiv.id = "splashBR";
+  splashDiv.onclick = onSplashClick;
+  document.body.appendChild(splashDiv);
+} else if (window.location.hostname.indexOf('betraisland') > -1) {
+  splashDiv = document.createElement("div");
+  splashDiv.id = "splashBI";
+  splashDiv.onclick = onSplashClick;
+  document.body.appendChild(splashDiv);
+} else {
+  splashDiv = document.createElement("div");
+  splashDiv.id = "splashYrPri";
+  splashDiv.onclick = onSplashClick;
+  document.body.appendChild(splashDiv);
+}
+
 function onSplashClick() {
-  var loadContainer = document.getElementById('splash');
-  loadContainer.parentNode.removeChild(loadContainer);
+  var loadContainer = document.getElementById('splashYrPri');
+  if (loadContainer) {
+    loadContainer.parentNode.removeChild(loadContainer);
+  }
+  loadContainer = document.getElementById('splashBR');
+  if (loadContainer) {
+    loadContainer.parentNode.removeChild(loadContainer);
+  }
+  loadContainer = document.getElementById('splashBI');
+  if (loadContainer) {
+    loadContainer.parentNode.removeChild(loadContainer);
+  }
   document.body.classList.remove('loading');
 }
