70fcf1bc5fabfb1e974b1094072b21a63e4b2a2f
Róbert Viðar Bjarnason <robert@citizens.is>
2016-05-08 19:41:47 +0000

    Tro to fix ie

--- server_api/api_app.js
+++ server_api/api_app.js
@@ -96,7 +96,7 @@ app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
 
-if (app.get('env') === 'development') {
+if (false && app.get('env') === 'development') {
   app.use(express.static(path.join(__dirname, '../client_app')));
 } else {
   app.use(express.static(path.join(__dirname, '../client_dist')));
