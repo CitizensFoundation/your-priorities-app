4e569ff6b39f9d49477cca717b578491c77b1ade
Róbert Viðar Bjarnason <robert@citizens.is>
2016-05-11 19:13:48 +0000

    Her

--- server_api/config/config.js
+++ server_api/config/config.js
@@ -1,6 +1,12 @@
 module.exports = {
     "production": {
-        "use_env_variable": "DATABASE_URL",
-        "dialect": "postgres",
+        "use_env_variable":"DB_CONNECTION_STRING",
+        "dialect":"postgres",
+        "ssl":true,
+        "dialectOptions":{
+            "ssl":{
+                "require":true
+            }
+        }
     }
 }
