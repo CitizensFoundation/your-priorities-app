cda44421c8bdc7e9e7fe43f1906fec537d2edc38
Róbert Viðar Bjarnason <robert@citizens.is>
2016-06-09 18:26:12 +0000

    Fixes for group access

--- server_api/scripts/fixGroupAccess.js
+++ server_api/scripts/fixGroupAccess.js
@@ -7,7 +7,7 @@ models.Group.findAll({where: {access: 1}}).then(function(groups) {
   async.eachSeries(groups, function (group, seriesCallback) {
     group.access = models.Group.ACCESS_OPEN_TO_COMMUNITY;
     group.save().then(function () {
-      console.log("Changed to open to community "+group.name)
+      console.log("Changed to open to community "+group.name);
       seriesCallback();
     })
   }, function (error) {
