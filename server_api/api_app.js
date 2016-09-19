98652ead93912b979fedad8d822bf5e4e8f933e6
Robert Bjarnason <Robert Bjarnason>
2016-09-19 00:28:36 +0000

    Added embedly to urls

--- server_api/api_app.js
+++ server_api/api_app.js
@@ -117,7 +117,7 @@ app.use(function (req, res, next) {
 
 app.use(function(req,res,next) {
   var ua = req.headers['user-agent'];
-  if (/^(facebookexternalhit)|(Twitterbot)|(Slackbot)|(Pinterest)/gi.test(ua)) {
+  if (/^(facebookexternalhit)|(Twitterbot)|(Slackbot)|(Embedly)|(Pinterest)/gi.test(ua)) {
     console.log(ua,' is a bot');
     nonSPArouter(req,res,next);
   } else {
