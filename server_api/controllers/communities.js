cc0bc5e4a62216c47808027384e86d38709c13bb
Robert Bjarnason <Robert Bjarnason>
2017-05-19 19:51:07 +0000

    Work on optional support for Facebook pixel

--- server_api/controllers/communities.js
+++ server_api/controllers/communities.js
@@ -250,6 +250,7 @@ var updateCommunityConfigParameters = function (req, community) {
   community.set('configuration.alternativeHeader', (req.body.alternativeHeader && req.body.alternativeHeader!="") ? req.body.alternativeHeader : null);
   community.set('configuration.disableDomainUpLink', (req.body.disableDomainUpLink && req.body.disableDomainUpLink!="") ? true : false);
   community.set('configuration.defaultLocationLongLat', (req.body.defaultLocationLongLat && req.body.defaultLocationLongLat!="") ? req.body.defaultLocationLongLat : null);
+  community.set('configuration.facebookPixelId', (req.body.facebookPixelId && req.body.facebookPixelId!="") ? req.body.facebookPixelId : null);
 };
 
 router.delete('/:communityId/:activityId/delete_activity', auth.can('edit community'), function(req, res) {
