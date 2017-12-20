d7c22ca8cada2450af37a05b639340b07ef6f037
Robert Bjarnason <robert.bjarnason@gmail.com>
2017-12-20 20:18:50 +0000

    Fallback language

--- client_app/src/app.js
+++ client_app/src/app.js
@@ -42,7 +42,7 @@
 
     var language = window.locale;
     i18next.use(i18nextXHRBackend).
-            init({ lng: language, backend: { loadPath: '/locales/{{lng}}/{{ns}}.json' } }, function(loaded) {
+            init({ lng: language, fallbackLng: 'en', backend: { loadPath: '/locales/{{lng}}/{{ns}}.json' } }, function(loaded) {
       window.i18nTranslation = i18next;
       if (typeof moment !== 'undefined' && moment ) {
         moment.locale(language);
