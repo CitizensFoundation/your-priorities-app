5fe3f9f6b63c54991172aa4ff7f0438e59a38d69
Robert Bjarnason <robert.bjarnason@gmail.com>
2018-02-21 00:19:28 +0000

    Custom build pipeline with cache busting

---
+++ client_app/gulpfile.js.tool.js
@@ -0,0 +1,13 @@
+const del = require('del');
+const gulp = require('gulp');
+const polymerBuildUtils = require('gulp-polymer-build-utils');
+const runSequence = require('run-sequence');
+gulp.task('default', cb => {
+  return runSequence('dist:prepare', 'inline-references', cb);
+});
+gulp.task('dist:prepare', ['dist:clean'], () => {
+  const config = require('./polymer.json');
+  return polymerBuildUtils.build(config)()
+    .pipe(gulp.dest('dist'));
+});
+gulp.task('dist:clean', () => del('dist'));
