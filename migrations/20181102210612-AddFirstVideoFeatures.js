ccb4d4deb326039f966678ab01e5fc65ae273c20
Robert Bjarnason <robert.bjarnason@gmail.com>
2018-11-03 18:10:31 +0000

    Work on videos

--- migrations/20181102210612-AddFirstVideoFeatures.js
+++ migrations/20181102210612-AddFirstVideoFeatures.js
@@ -17,24 +17,181 @@ module.exports = {
           updated_at: {
             type: Sequelize.DATE
           },
-          index_key: {
-            type: Sequelize.STRING,
-            allowNull: false
-          },
+          name: Sequelize.STRING,
+          description: Sequelize.TEXT,
           content: {
             type: Sequelize.TEXT,
             allowNull: false
-          }
+          },
+          meta: DataType.JSONB,
+          formats: DataType.JSONB,
+          viewable: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+          ip_address: { type: DataTypes.STRING, allowNull: false },
+          user_agent: { type: DataTypes.TEXT, allowNull: false },
+          deleted: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
         }
       ),
-      await queryInterface.addIndex('translation_cache', ['index_key']),
-      await queryInterface.addColumn('posts','language', { type: Sequelize.STRING, allowNull: true }),
-      await queryInterface.addColumn('points','language', { type: Sequelize.STRING, allowNull: true }),
-      await queryInterface.addColumn('domains','language', { type: Sequelize.STRING, allowNull: true }),
-      await queryInterface.addColumn('communities','language', { type: Sequelize.STRING, allowNull: true }),
-      await queryInterface.addColumn('groups','language', { type: Sequelize.STRING, allowNull: true }),
-      await queryInterface.addColumn('categories','language', { type: Sequelize.STRING, allowNull: true }),
-      await queryInterface.addColumn('post_status_changes','language', { type: Sequelize.STRING, allowNull: true })
+      await queryInterface.addIndex('videos', {
+        fields: ['meta'],
+        using: 'gin',
+        operator: 'jsonb_path_ops'
+      }),
+      await queryInterface.addIndex('videos', {
+        fields: ['formats'],
+        using: 'gin',
+        operator: 'jsonb_path_ops'
+      }),
+      await queryInterface.addIndex('videos', ['user_id', 'viewable', 'deleted']),
+      await queryInterface.createTable(
+        'post_videos',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          created_at: {
+            type: Sequelize.DATE
+          },
+          updated_at: {
+            type: Sequelize.DATE
+          },
+          videoId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'videos',
+              key: 'id'
+            },
+          },
+          postId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'posts',
+              key: 'id'
+            },
+          },
+        }
+      ),
+      await queryInterface.createTable(
+        'user_profile_videos',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          created_at: {
+            type: Sequelize.DATE
+          },
+          updated_at: {
+            type: Sequelize.DATE
+          },
+          videoId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'videos',
+              key: 'id'
+            },
+          },
+          userId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'users',
+              key: 'id'
+            },
+          },
+        }
+      ),
+      await queryInterface.createTable(
+        'community_logo_videos',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          created_at: {
+            type: Sequelize.DATE
+          },
+          updated_at: {
+            type: Sequelize.DATE
+          },
+          videoId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'videos',
+              key: 'id'
+            },
+          },
+          communityId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'communities',
+              key: 'id'
+            },
+          },
+        }
+      ),
+      await queryInterface.createTable(
+        'group_logo_videos',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          created_at: {
+            type: Sequelize.DATE
+          },
+          updated_at: {
+            type: Sequelize.DATE
+          },
+          videoId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'videos',
+              key: 'id'
+            },
+          },
+          groupId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'groups',
+              key: 'id'
+            },
+          },
+        }
+      ),
+      await queryInterface.createTable(
+        'domain_logo_videos',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          created_at: {
+            type: Sequelize.DATE
+          },
+          updated_at: {
+            type: Sequelize.DATE
+          },
+          videoId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'videos',
+              key: 'id'
+            },
+          },
+          domainId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'domains',
+              key: 'id'
+            },
+          },
+        }
+      ),
     ]
   },
 
