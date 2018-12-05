1d3cd40c93ae5090c3a3eb16e29759db794e7020
Robert Bjarnason <robert.bjarnason@gmail.com>
2018-12-05 20:09:08 +0000

    Enable hiding of news on domain level

--- server_api/controllers/domains.js
+++ server_api/controllers/domains.js
@@ -588,6 +588,7 @@ router.put('/:id', auth.can('edit domain'), function(req, res) {
 
       domain.set('configuration.appHomeScreenShortName', (req.body.appHomeScreenShortName && req.body.appHomeScreenShortName!="")? req.body.appHomeScreenShortName : null);
       domain.set('configuration.useVideoCover', truthValueFromBody(req.body.useVideoCover));
+      domain.set('configuration.hideDomainNews', truthValueFromBody(req.body.hideDomainNews));
 
       domain.name = req.body.name;
       domain.description = req.body.description;
