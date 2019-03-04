ffd130c61f1e7c95d4f3bb9bcacace48a84d516f
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-03-04 18:56:07 +0000

    Fix category edit

--- server_api/controllers/posts.js
+++ server_api/controllers/posts.js
@@ -170,7 +170,7 @@ router.get('/:id', auth.can('view post'), function(req, res) {
       id: req.params.id
     },
     attributes: ['id','name','description','public_data','status','content_type','official_status','counter_endorsements_up','cover_media_type',
-      'counter_endorsements_down','group_id','language','counter_points','counter_flags','location','created_at'],
+                 'counter_endorsements_down','group_id','language','counter_points','counter_flags','location','created_at','category_id'],
     order: [
       [ { model: models.Image, as: 'PostHeaderImages' } ,'updated_at', 'asc' ],
       [ { model: models.Category }, { model: models.Image, as: 'CategoryIconImages' } ,'updated_at', 'asc' ],
