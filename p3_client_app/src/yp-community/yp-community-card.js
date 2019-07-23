747567e514404653f4182fb4c1320d118e688b8d
Alexander <alexander.gautason@gmail.com>
2019-07-23 17:56:39 +0000

    converting

--- p3_client_app/src/yp-community/yp-community-card.js
+++ p3_client_app/src/yp-community/yp-community-card.js
@@ -13,6 +13,7 @@ import { ypMediaFormatsBehavior } from '../yp-behaviors/yp-media-formats-behavio
 import '../yp-magic-text/yp-magic-text.js';
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+
 Polymer({
   _template: html`
     <style include="iron-flex iron-flex-alignment">
