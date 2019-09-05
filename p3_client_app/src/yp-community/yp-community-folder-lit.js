66c2d82efa64a2c11ee6b00bb718c2a4fddff1c8
Alexander <alexander.gautason@gmail.com>
2019-09-05 16:20:26 +0000

    added

---
+++ p3_client_app/src/yp-community/yp-community-folder-lit.js
@@ -0,0 +1,285 @@
+import '@polymer/polymer/polymer-legacy.js';
+import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
+import 'lite-signal/lite-signal.js';
+import '@polymer/iron-pages/iron-pages.js';
+import '@polymer/paper-tabs/paper-tab.js';
+import '@polymer/paper-tabs/paper-tabs.js';
+import '@polymer/app-route/app-route.js';
+import { ypLanguageBehavior } from '../yp-behaviors/yp-language-behavior.js';
+import { CollectionHelpers } from '../yp-behaviors/collection-helpers.js';
+import { ypLoggedInUserBehavior } from '../yp-behaviors/yp-logged-in-user-behavior.js';
+import { ypDetectOldiOs } from '../yp-behaviors/yp-detect-old-ios.js';
+import { ypGotoBehavior } from '../yp-behaviors/yp-goto-behavior.js';
+import { ypMediaFormatsBehavior } from '../yp-behaviors/yp-media-formats-behavior.js';
+import '../ac-activities/ac-activities.js';
+import { ypThemeBehavior } from '../yp-theme/yp-theme-behavior.js';
+import '../yp-ajax/yp-ajax.js';
+import '../yp-page/yp-page.js';
+import { CommunityCollectionBehaviors } from './yp-community-collection-behaviors.js';
+import './yp-community-grid.js';
+import './yp-community-header.js';
+import './yp-community-large-card.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { AccessHelpers } from '../yp-behaviors/access-helpers.js';
+import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
+class YpCommunityFolderLit extends YpBaseElement {
+  static get properties() {
+    return {
+      idRoute: Object,
+      tabRoute: Object,
+      idRouteData: Object,
+      tabRouteData: Object,
+      createFabIcon: {
+        type: String,
+        value: null,
+        notify: true
+      },
+  
+      communityFolderId: {
+        type: Number,
+        value: null,
+        observer: "_communityFolderIdChanged"
+      },
+  
+      communityFolder: {
+        type: Object
+      },
+  
+      mapActive: {
+        type: Boolean,
+        value: false
+      },
+  
+      locationHidden: {
+        type: Boolean,
+        value: false
+      },
+  
+      useAlternativeHeader: {
+        type: Boolean,
+        value: false
+      },
+  
+      isOldiOs: {
+        type: Boolean,
+        computed: '_isOldiOs(communityFolderId)'
+      },
+  
+      useNormalHeader: {
+        type: Boolean,
+        value: true
+      }
+    }          
+  }
+  
+  static get styles() {
+    return [
+      css`    
+  }
+    .card {
+        padding: 16px;
+    }
+
+      yp-ajax {
+        background-color: var(--primary-background-color) !important;
+      }
+
+      .archivedText {
+        font-size: 26px;
+        color: #333;
+      }
+
+      .minHeightSection {
+        min-height: 450px;
+      }
+
+      #paper_tabs[apple] {
+        margin-top: 42px;
+        margin-bottom: 8px;
+      }
+
+      [hidden] {
+        display: none !important;
+      }
+    
+      `, YpFlexLayout]
+    }
+  
+  render() {
+    return html`
+    <yp-page id="page" create-fab-icon="${this.createFabIcon}" hide-all-tabs="" create-fab-title="${t(this.group.add)}" on-yp-create-fab-tap="_newGroup">
+
+      <yp-community-large-card id="communityCard" slot="largeCard" class="largeCard card" community="${this.communityFolder}" on-update-community="_refreshAjax"></yp-community-large-card>
+
+      <div class="layout horizontal center-center wrap" slot="tabPages">
+        <yp-community-grid featured-communities="${this.featuredCommunities}" active-communities="${this.activeCommunities}" archived-communities="${this.archivedCommunities}" hide-add="${!this.createFabIcon}" on-add-new-community="_newCommunity">
+        </yp-community-grid>
+      </div>
+    </yp-page>
+
+    <lite-signal on-lite-signal-yp-language="_languageEvent"></lite-signal>
+    <lite-signal on-lite-signal-logged-in="_userLoggedIn"></lite-signal>
+    <lite-signal on-lite-signal-got-admin-rights="_gotAdminRights"></lite-signal>
+
+    <app-route route="${this.idRoute}" pattern="/:id" data="${this.idRouteData}" tail="${this.tabRoute}">
+    </app-route>
+
+    <app-route route="${this.tabRoute}" pattern="/:tabName" data="${this.tabRouteData}">
+    </app-route>
+
+    <yp-ajax id="ajax" url="/api/domains" on-response="_response"></yp-ajax>
+    <yp-ajax id="pagesAjax" on-response="_pagesResponse"></yp-ajax>
+`
+}
+
+  _userLoggedIn(user) {
+    if (user) {
+      if (this.communityFolder && window.location.href.indexOf("/community_folder/") > -1) {
+        this.$$('#ajax').generateRequest();
+      }
+    }
+  }
+
+  _routeIdChanged(newId) {
+    if (newId) {
+      this.set('communityFolderId', newId);
+    }
+  }
+
+  _hideEdit() {
+    if (!this.communityFolder)
+      return true;
+
+    if (!window.appUser.loggedIn())
+      return true;
+
+    return (window.appUser.user.id!=this.communityFolder.user_id);
+  }
+
+  _communityHeaderUrl(communityFolder) {
+    return this.getImageFormatUrl(communityFolder.CommunityHeaderImages, 2);
+  }
+
+  _communityFolderIdChanged(newValue, oldValue) {
+    if (newValue) {
+      this.set("communityFolder", null);
+      this.set("featuredCommunities",null);
+      this.set("activeCommuntities",null);
+      this.set("archivedCommunities",null);
+      this._getCommunityFolder();
+    }
+  }
+
+  _getCommunityFolder() {
+    this.$$('#ajax').url = '/api/communities/' + this.communityFolderId + '/communityFolders';
+    this.$$('#ajax').retryMethodAfter401Login = this._getCommunityFolder.bind(this);
+    this.$$('#ajax').generateRequest();
+  }
+
+  _refreshAjax() {
+    this.async(function () {
+      this.$$('#ajax').generateRequest();
+    }, 100);
+  }
+
+  _newCommunity() {
+    window.appGlobals.activity('open', 'newCommunity');
+    dom(document).querySelector('yp-app').getDialogAsync("communityEdit", function (dialog) {
+      dialog.setup(null, true, this._refreshAjax.bind(this));
+      dialog.open('new', {domainId: this.domainId, communityFolderId: this.communityFolderId } );
+    }.bind(this));
+  }
+
+  _pagesResponse(event, detail) {
+    this.fire('yp-set-pages', detail.response);
+  }
+
+  _response(event, detail, sender) {
+    this.set('communityFolder', detail.response);
+
+    if (!this.communityFolder.is_community_folder) {
+      this.redirectTo("/community/"+this.community.id);
+    } else {
+      this.refresh();
+
+      this.set('createFabIcon', null);
+
+      /*if (false && !this.communityFolder.Domain.only_admins_can_create_communities || this.checkDomainAccess(this.communityFolder.Domain)) {
+        this.set('createFabIcon', 'group-work');
+      } else {
+      }*/
+
+      var url = this._communityHeaderUrl(this.communityFolder);
+
+      this.setupCommunities(this.communityFolder.Communities);
+    }
+  }
+
+  _gotAdminRights(event, detail) {
+    if (detail && detail>0) {
+      if (this.checkCommunityAccess(this.communityFolder)) {
+        this.set('createFabIcon', 'add');
+      }
+    }
+  }
+
+  refresh() {
+    if (this.communityFolder) {
+      this.fire('yp-set-home-link', { type: 'communityFolder', id: this.communityFolder.id, name: this.communityFolder.name });
+
+      if (this.communityFolder.theme_id!=null) {
+        this.setTheme(this.communityFolder.theme_id);
+      } else if (this.communityFolder.Domain.theme_id!=null) {
+        this.setTheme(this.communityFolder.Domain.theme_id);
+      }
+
+      if (this.communityFolder.default_locale!=null) {
+        window.appGlobals.changeLocaleIfNeeded(this.communityFolder.default_locale);
+      }
+
+      if (this.communityFolder.CommunityHeaderImages && this.communityFolder.CommunityHeaderImages.length>0) {
+        this.$.page.setupTopHeaderImage(this.communityFolder.CommunityHeaderImages);
+      } else {
+        this.$.page.setupTopHeaderImage(null);
+      }
+
+      if (window.location.href.indexOf("/community_folder") > -1) {
+        var backPath, headerTitle, headerDescription;
+        if (this.communityFolder.CommunityFolder) {
+          backPath = "/community_folder/" + this.communityFolder.CommunityFolder.id;
+          headerTitle = this.communityFolder.CommunityFolder.name;
+          headerDescription = this.communityFolder.CommunityFolder.description;
+        } else {
+          backPath = "/domain/" + this.communityFolder.domain_id;
+          headerTitle = this.communityFolder.Domain.name;
+          headerDescription = this.communityFolder.Domain.description;
+        }
+        this.fire("change-header", {
+          headerTitle: headerTitle,
+          headerDescription: headerDescription,
+          headerIcon: "group-work",
+          disableDomainUpLink: false,
+          documentTitle: this.communityFolder.name,
+          backPath: backPath
+        });
+      }
+      this.$.pagesAjax.url = "/api/domains/"+this.communityFolder.Domain.id+"/pages";
+      this.$.pagesAjax.generateRequest();
+      window.appGlobals.setAnonymousGroupStatus(null);
+      window.appGlobals.disableFacebookLoginForGroup = false;
+      window.appGlobals.externalGoalTriggerUrl = null;
+      window.appGlobals.currentGroupForceSaml = false;
+      window.appGlobals.currentGroup = null;
+
+      if (this.communityFolder.configuration && this.communityFolder.configuration.signupTermsPageId &&
+        this.communityFolder.configuration.signupTermsPageId!=-1) {
+        window.appGlobals.signupTermsPageId = this.communityFolder.configuration.signupTermsPageId;
+      } else {
+        window.appGlobals.signupTermsPageId = null;
+      }
+    }
+  }
+}
+
+window.customElements.define('yp-community-folder-lit', YpCommunityFolderLit)
---
+++ p3_client_app/src/yp-group/yp-group-grid-lit.js
@@ -0,0 +1,121 @@
+import '@polymer/polymer/polymer-legacy.js';
+import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
+import 'lite-signal/lite-signal.js';
+import { ypLanguageBehavior } from '../yp-behaviors/yp-language-behavior.js';
+import { ypIronListBehavior } from '../yp-behaviors/yp-iron-list-behavior.js';
+import { ypCardMouseBehavior } from '../yp-behaviors/yp-card-mouse-behavior.js';
+import './yp-group-card.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { YpBaseElement } from '../yp-base-element.js';
+
+class YpGroupGridLit extends YpBaseElement {
+  static get properties() {
+    return {
+      featuredGroups: Array,
+      archivedGroups: Array,
+      activeGroups: Array,
+        hideAdd: {
+          type: Boolean,
+          value: false
+        },
+
+        wide: {
+            type: Boolean,
+            value: false
+          },
+
+      scrollOffset: {
+            type: Number,
+            computed: '_scrollOffset(wide, featuredGroups, activeGroups)'
+      }
+    }
+  }
+    
+  static get styles() {
+      return [
+        css`    
+  }
+        .groupCard {
+        padding: 0;
+        padding-top: 16px;
+      }
+
+      .groupCard[wide-padding] {
+        padding: 16px !important;
+      }
+
+      iron-list {
+        height: 100vh;
+      }
+
+      [hidden] {
+        display: none !important;
+      }
+
+      :focus {
+        outline: none;
+      }
+
+      @media (max-width: 1199px) {
+        .groupCard {
+        }
+      }
+    `, YpFlexLayout]
+  }
+render() {
+    return html `    
+    <lite-signal on-lite-signal-yp-language="_languageEvent"></lite-signal>
+    <iron-media-query query="(min-width: 1024px)" query-matches="${this.wide}"></iron-media-query>
+
+    <div class="layout horizontal center-center">
+      <iron-list id="ironList" scroll-offset="${this.scrollOffset}" items="${this.activeGroups}" as="group" scroll-target="document" grid="${this.wide}">
+        <template>
+          <div class="groupCard layout vertical center-center" tabindex="${this.tabIndex}" wide-padding="${this.wide}">
+            <yp-group-card-lit wide-padding="${this.wide}" group="${this.group}" on-mouseover="cardMouseOver" on-mouseout="cardMouseOut"></yp-group-card-lit>
+          </div>
+        </template>
+      </iron-list>
+    </div>
+`
+}
+_newGroup() {
+    this.fire('add-new-group');
+  }
+
+  _scrollOffset(wide, featuredGroups) {
+    var list = this.$.ironList;
+    if (list) {
+      var offset = list.offsetTop;
+      offset -= 75;
+      if (list.offsetTop>0 && offset>0) {
+        console.info("Group list scroll offset: "+offset);
+        return offset;
+      } else {
+        if (wide)
+          offset = 390;
+        else
+          offset = 450;
+        console.info("Group list (manual) scroll offset: "+offset);
+        return offset;
+      }
+    } else {
+      console.warn("No group list for scroll offset");
+      return null;
+    }
+  }
+
+  scrollToItem(item) {
+    console.log("Group grid scrolling to item");
+    this.$.ironList.scrollToItem(item);
+    document.dispatchEvent(
+      new CustomEvent("lite-signal", {
+        bubbles: true,
+        compose: true,
+        detail: { name: 'yp-refresh-activities-scroll-threshold', data: {} }
+      })
+    )
+  }
+}
+
+window.customElement.define('yp-group-grid-lit', YpGroupGridLit)
