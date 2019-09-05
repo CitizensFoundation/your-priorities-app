04da8ac0f3578bf49de9e2091b9c9decf90501f3
Alexander <alexander.gautason@gmail.com>
2019-09-05 18:09:15 +0000

    fixing

--- p3_client_app/src/yp-group/yp-group-stats-lit.js
+++ p3_client_app/src/yp-group/yp-group-stats-lit.js
@@ -1,23 +1,15 @@
-import '@polymer/polymer/polymer-legacy.js';
-import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
-import 'lite-signal/lite-signal.js';
 import '../yp-app-globals/yp-app-icons.js';
-import { ypLanguageBehavior } from '../yp-behaviors/yp-language-behavior.js';
-import { ypNumberFormatBehavior } from '../yp-behaviors/yp-number-format-behavior.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
-import { YpBaseElement } from '../yp-base-element.js';
 import { html, css } from 'lit-element';
+import { YpBaseElement } from '../yp-base-element.js';
 import { YpFlexLayout } from '../yp-flex-layout.js';
-
-  class YpGroupStatsLit extends YpBaseElement {
-    static get properties() {
-      return {
-        group: {
-          type: Object
-        }
+class YpGroupStatsLit extends YpBaseElement {
+  static get properties() {
+    return {
+      group: {
+        type: Object
       }
     }
+  }
   
 
   static get styles() {
