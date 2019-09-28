948ad56243c9fec06eef9cf95d104af44847c39a
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-09-28 12:53:01 +0000

    List & change languages scripts

---
+++ server_api/scripts/changeLanguage.js
@@ -0,0 +1,48 @@
+var models = require('../models');
+var async = require('async');
+var ip = require('ip');
+var _ = require('lodash');
+
+const modelType = process.argv[2];
+const modelId = process.argv[3];
+const language = process.argv[4];
+
+if (modelType==="post") {
+  models.Post.find({
+    where: {
+      id: modelId
+    },
+    attributes: ['id','language']
+  }).then((post)=>{
+    if (post) {
+      post.language = language;
+      post.save().then(()=> {
+        console.log("Done");
+        process.exit();
+      })
+    } else {
+      console.error("No post");
+      process.exit();
+    }
+  });
+} else if (modelType==="point") {
+  models.Point.find({
+    where: {
+      id: modelId
+    },
+    attributes: ['id','language']
+  }).then((point)=>{
+    if (point) {
+      point.language = language;
+      point.save().then(()=> {
+        console.log("Done");
+        process.exit();
+      })
+    } else {
+      console.error("No point");
+      process.exit();
+    }
+  });
+} else {
+  process.exit();
+}
