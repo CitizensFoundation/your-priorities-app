73f2f5ad23fedc63ea3d24b845a94f3b742b5aa5
Alexander <alexander.gautason@gmail.com>
2019-12-09 17:24:45 +0000

    BOGOS

---
+++ p3_client_app/src/ac-activities/ac-activities-lit.js
@@ -0,0 +1,621 @@
+import '@polymer/polymer/polymer-legacy.js';
+import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
+import '@polymer/iron-list/iron-list.js';
+import '@polymer/iron-scroll-threshold/iron-scroll-threshold.js';
+import 'lite-signal/lite-signal.js';
+import '@polymer/paper-material/paper-material.js';
+import { ypLanguageBehavior } from '../yp-behaviors/yp-language-behavior.js';
+import { ypLoggedInUserBehavior } from '../yp-behaviors/yp-logged-in-user-behavior.js';
+import { AccessHelpers } from '../yp-behaviors/access-helpers.js';
+import { ypIronListBehavior } from '../yp-behaviors/yp-iron-list-behavior.js';
+import '../yp-ajax/yp-ajax.js';
+import '../yp-point/yp-point-news-story-edit.js';
+import './ac-activity.js';
+import './ac-activity-recommended-posts.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
+
+class AcActivitiesLit extends YpBaseElement {
+  static get properties() {
+    return {
+      disableNewPosts: {
+        type: Boolean,
+        value: false
+      },
+  
+      activities: {
+        type: Array,
+        notify: true
+      },
+  
+      domainId: {
+        type: Number,
+        observer: "_domainIdChanged"
+      },
+  
+      communityId: {
+        type: Number,
+        observer: "_communityIdChanged"
+      },
+  
+      groupId: {
+        type: Number,
+        observer: "_groupIdChanged"
+      },
+  
+      postId: {
+        type: Number,
+        observer: "_postIdChanged"
+      },
+  
+      postGroupId: {
+        type: Number
+      },
+  
+      // 'activities' and 'news_feed'
+      mode: {
+        type: String,
+        value: "activities"
+      },
+  
+      url: {
+        type: String
+      },
+  
+      oldestProcessedActivityAt: {
+        type: Date
+      },
+  
+      latestProcessedActivityAt: {
+        type: Date
+      },
+  
+      activityIdToDelete: {
+        type: Number
+      },
+  
+      wide: {
+        type: Boolean,
+        value: false
+      },
+  
+      selectedTab: {
+        type: String
+      },
+  
+      recommendedPosts: {
+        type: Array,
+        value: null
+      },
+  
+      noRecommendedPosts: {
+        type: Boolean,
+        value: true
+      },
+  
+      skipIronListWidth: {
+        type: Boolean,
+        computed: '_skipIronListWidth(wide)'
+      },
+  
+      ironListResizeScrollThreshold: {
+        type: Number,
+        computed: '_ironListResizeScrollThreshold(wide)'
+      },
+  
+      ironListPaddingTop: {
+        type: Number,
+        computed: '_ironListPaddingTop(wide, groupId, hasLoggedInUser, selectedTab)'
+      },
+  
+      wideListOffset: {
+        type: String,
+        computed: '_wideListOffset(groupId)'
+      },
+  
+      hasLoggedInUser: {
+        type: Boolean,
+        computed: '_hasLoggedInUser(loggedInUser)'
+      },
+  
+      gotInitialData: {
+        type: Boolean,
+        value: false
+      }
+    }
+  }
+
+  static get styles() {
+    return [
+      css`
+
+      iron-list {
+        height: 100vh;
+      }
+
+      .addNewsBox {
+        background-color: #FFF;
+        width: 550px;
+        height: 100%;
+        padding-left: 16px;
+        padding-right: 16px;
+        margin-top: 16px;
+        margin-left: 16px;
+        margin-right: 16px;
+      }
+
+      @media (max-width: 600px) {
+        .addNewsBox {
+          width: 100%;
+          height: 100%;
+          margin-bottom: 8px;
+          margin-top: 8px;
+          margin-left: 0;
+          margin-right: 0;
+          width: -webkit-calc(100% - 16px);
+          width:    -moz-calc(100% - 16px);
+          width:         calc(100% - 16px);
+        }
+      }
+
+      @media (max-width: 340px) {
+        .addNewsBox {
+          width: 100%;
+          height: 100%;
+          margin-bottom: 8px;
+          margin-top: 8px;
+          margin-left: 0;
+          margin-right: 0;
+          width: -webkit-calc(100% - 36px);
+          width:    -moz-calc(100% - 36px);
+          width:         calc(100% - 36px);
+        }
+      }
+
+      .activityContainer {
+        width: 550px;
+        margin: 0;
+        padding: 0;
+      }
+
+      @media (max-width: 600px) {
+        .activityContainer {
+          width: 100%;
+        }
+      }
+
+      .recommendedPosts[not-active] {
+        display: none;
+      }
+
+      .recommendedPosts[small] {
+        display: none;
+      }
+
+
+      .mainActivityContent {
+        height: 100% !important;
+      }
+
+      .headerUserImage {
+        padding-top: 16px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+
+      paper-button {
+        color: var(--accent-color);
+      }
+
+      iron-icon {
+        width: 48px;
+        height: 48px;
+        padding-top: 14px;
+      }
+
+
+      .createdAt {
+        color: #777;
+        margin-top: 16px;
+        font-size: 14px;
+      }
+
+      yp-ajax {
+        background-color: var(--primary-background-color);
+      }
+
+      .deleteIcon {
+        position: absolute;
+        right: 8px;
+        bottom: 8px;
+        color: #ddd;
+      }
+
+      .withCursor {
+        cursor: pointer;
+      }
+
+      .activityContainer {
+        width: 100%;
+      }
+
+      @media (max-width: 960px) {
+        .recommendedPosts {
+          display: none !important;
+        }
+      }
+
+      .topLevelActivitiesContainer[wide] {
+      }
+
+      [hidden] {
+        display: none !important;
+      }
+
+      .spinnerContainer {
+        margin-top: 32px;
+      }
+
+      .topSpinnerContainer {
+        margin-top: 16px;
+      }
+
+      :focus {
+        outline: none;
+      }
+
+      .notLoggedInButton {
+        margin-top: 8px;
+        width: 250px;
+        background-color: #FFF;
+        margin-bottom: 8px;
+        text-align: center;
+      }
+    `, YpflexLayout]
+  }
+
+  render() {
+    return html`
+    ${this.activities ? html`
+    <div class="layout horizontal topLevelActivitiesContainer layout-center-center" wide="${this.wide}">
+      <div class="layout vertical self-start">
+        <template is="dom-if" if="${this.loggedInUser}">
+          <paper-material .loggedInUser="${this.hasLoggedInUser}" elevation="1" class="layout horizontal addNewsBox">
+            <yp-point-news-story-edit .domainId="${this.domainId}" .communityId="${this.communityId}" groupId="${this.groupId}" .postGroupId="${this.postGroupId}" .postId="${this.postId}" @refresh="${this.loadNewData}">
+            </yp-point-news-story-edit>
+          </paper-material>
+        </template>
+        <template is="dom-if" if="[[!loggedInUser]]">
+          <div class="layout vertical center-center">
+            <paper-button raised class="layout horizontal notLoggedInButton" @tap="${this._openLogin}">
+              ${this.t('loginToShareALink')}
+            </paper-button>
+          </div>
+        </template>
+
+        <div class="layout horizontal center-center topSpinnerContainer" ?hidden="${this.gotInitialData}">
+          <yp-ajax id="ajax" large-spinner @response="${this._activitiesResponse}"></yp-ajax>
+        </div>
+
+        <iron-list id="ironList" .scrollOffset="${this.ironListPaddingTop}" .scrollTarget="document" .items="${this.activities}" as="activity">
+          <template>
+            <div tabindex ="${this.tabIndex}" class="layout vertical center-center">
+              <ac-activity .hasLoggedInUser="${this.hasLoggedInUser}" class="activityContainer" .activity="${this.activity}" .postId="${this.postId}" groupId="${this.groupId}" .communityId="${this.communityId}" domainId="${this.domainId}" @ak-delete-activity="${this._deleteActivity}"></ac-activity>
+            </div>
+          </template>
+        </iron-list>
+        <div class="layout horizontal center-center spinnerContainer">
+          <yp-ajax id="deleteActivityAjax" .method="DELETE" large-spinner @response="${this._activityDeletedResponse}"></yp-ajax>
+        </div>
+      </div>
+      <div class="layout vertical self-start recommendedPosts" ?notActive="${this.noRecommendedPosts}" small="${!this.wide}" ?hidden="${!this.recommendedPosts}">
+        <ac-activity-recommended-posts id="recommendedPosts" .recommendedPosts="${this.recommendedPosts}" class="layout vertical"></ac-activity-recommended-posts>
+        <div class="layout horizontal center-center">
+          <yp-ajax id="recommendationAjax" large-spinner @response="${this._recommendedPostsResponse}"></yp-ajax>
+        </div>
+      </div>
+    </div>
+
+    <iron-media-query query="(min-width: 600px)" query-matches="${this.wide}"></iron-media-query>
+
+    <lite-signal @lite-signal-logged-in="${this._userLoggedIn}"></lite-signal>
+    <lite-signal @lite-signal-yp-language="${this._languageEvent}"></lite-signal>
+    <lite-signal @lite-signal-yp-refresh-activities-scroll-threshold="${this._clearScrollThreshold}"></lite-signal>
+
+    <iron-scroll-threshold id="scrollTheshold" .lowerThreshold="450" @lower-threshold="${this._loadMoreData}" .scrollTarget="document">
+    </iron-scroll-threshold>
+` : html``}
+`
+  }
+
+/*
+  behaviors: [
+    ypLanguageBehavior,
+    ypLoggedInUserBehavior,
+    AccessHelpers,
+    ypIronListBehavior
+  ],
+
+  listeners: {
+    'yp-point-deleted': '_pointDeleted'
+  },
+*/
+
+  _openLogin() {
+    this.fire('yp-open-login');
+  }
+
+  _pointDeleted(event, detail) {
+    for (var i = 0; i < this.activities.length; i++) {
+      if (this.activities[i].Point) {
+        if (this.activities[i].Point.id==detail.pointId) {
+          this._removeActivityId(this.activities[i].id);
+        }
+      }
+    }
+  }
+
+  _hasLoggedInUser(user) {
+    if (user) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  _wideListOffset(groupId) {
+    if (groupId) {
+      return "800";
+    } else {
+      return "415"
+    }
+  }
+
+  _ironListResizeScrollThreshold(wide) {
+    if (wide) {
+      return 800;
+    } else {
+      return 300;
+    }
+  }
+
+  _ironListPaddingTop(wide, groupId, hasLoggedInUser, selectedTab) {
+    var offset = this.$.ironList.offsetTop;
+    offset -= 75;
+
+    if (!hasLoggedInUser && !groupId)
+      offset -= 75;
+
+    if (offset>0) {
+      console.info("News scroll offset: "+offset);
+      return offset;
+    } else {
+      if (groupId) {
+        if (wide) {
+          offset = hasLoggedInUser? 700 : 580;
+        } else {
+          offset = hasLoggedInUser? 950 :  690;
+        }
+      } else {
+        if (wide) {
+          offset = hasLoggedInUser? 600 : 400;
+        } else {
+          offset = hasLoggedInUser? 700 : 610;
+        }
+      }
+      console.info("News (manual) scroll offset: "+offset);
+      return offset;
+    }
+  }
+
+  _skipIronListWidth(wide) {
+    if (wide) {
+      var list = this.$$("#ironList");
+      list.style.width = '600px';
+      list.updateViewportBoundaries();
+      this.async(function () {
+        list.notifyResize();
+      }, 50);
+    }
+    return wide;
+  }
+
+  _activityDeletedResponse(event, detail) {
+    this._removeActivityId(detail.response.activityId);
+  }
+
+  _removeActivityId(activityId) {
+    for (var i = 0; i < this.activities.length; i++) {
+      if (this.activities[i].id == activityId) {
+        this.splice('activities', i, 1);
+      }
+    }
+    this.$$("#ironList").fire("iron-resize");
+  }
+
+  _deleteActivity(event, detail) {
+    this.set('activityIdToDelete', detail.id);
+    dom(document).querySelector('yp-app').getDialogAsync("confirmationDialog", function (dialog) {
+      dialog.open(this.t('activity.confirmDelete'), this._reallyDelete.bind(this));
+    }.bind(this));
+  }
+
+  _reallyDelete() {
+    if (this.domainId) {
+      this.$.deleteActivityAjax.url = "/api/domains/"+this.domainId+"/"+this.activityIdToDelete+"/delete_activity";
+    } else if (this.communityId) {
+      this.$.deleteActivityAjax.url = "/api/communities/"+this.communityId+"/"+this.activityIdToDelete+"/delete_activity";
+    } else if (this.groupId) {
+      this.$.deleteActivityAjax.url = "/api/groups/"+this.groupId+"/"+this.activityIdToDelete+"/delete_activity";
+    } else if (this.postId) {
+      this.$.deleteActivityAjax.url = "/api/posts/"+this.postId+"/"+this.activityIdToDelete+"/delete_activity";
+    }
+    this.$.deleteActivityAjax.body = {};
+    this.$.deleteActivityAjax.generateRequest();
+    this.set('activityIdToDelete', null);
+  }
+
+  _generateRequest(typeId, typeName) {
+    if (typeId) {
+      this.set('activities', []);
+      this.set('oldestProcessedActivityAt', null);
+      this.set('noRecommendedPosts', true);
+
+      //TODO: Add a minimum threshold of filtering before enabling dynamic news_feeds again
+      if (false && window.appUser && window.appUser.user && !this.postId) {
+        this.mode = 'news_feeds';
+      } else {
+        this.mode = 'activities';
+      }
+
+      this.set('url', '/api/'+this.mode+'/' + typeName + '/' + typeId);
+      this.$.ajax.url = this.url;
+      this.$.ajax.generateRequest();
+      if (typeName!='posts') {
+        this.$.recommendationAjax.url = '/api/recommendations/' + typeName + '/' + typeId;
+        this.$.recommendationAjax.generateRequest();
+      }
+    }
+  }
+
+  _loadMoreData() {
+    this.async(function () {
+      console.log("_loadMoreData for scroll");
+      if (this.$$("#ironList").offsetWidth > 0 && this.$$("#ironList").offsetHeight > 0) {
+        console.log("_loadMoreData for scroll 2 url: "+this.url+" moreToLoad: "+this.moreToLoad);
+        if (this.url!='' && this.moreToLoad && this.oldestProcessedActivityAt) {
+          console.log("_loadMoreData for scroll 3");
+          this.set('moreToLoad', false);
+          console.info("_loadMoreData for scroll for domainId: "+this.domainId+" communityId: "+this.communityId+" groupId: "+this.groupId+" postId: "+this.postId);
+          this.$.ajax.url = this.url + '?beforeDate='+this.oldestProcessedActivityAt;
+          this.$.ajax.generateRequest();
+        }
+      } else {
+        console.warn("NOT VISIBLE for domainId: "+this.domainId+" communityId: "+this.communityId+" groupId: "+this.groupId+" postId: "+this.postId);
+      }
+    });
+  }
+
+  loadNewData() {
+    if (this.url!='' && this.latestProcessedActivityAt) {
+      this.$.ajax.url = this.url + '?afterDate='+this.latestProcessedActivityAt;
+      this.$.ajax.generateRequest();
+    } else if (!this.latestProcessedActivityAt) {
+      this.$.ajax.url = this.url;
+      this.$.ajax.generateRequest();
+    }
+  }
+
+  _domainIdChanged(newValue) {
+    this.set('activities', null);
+    this.set('recommendedPosts', null);
+    this._generateRequest(newValue, 'domains');
+  }
+
+  _communityIdChanged(newValue) {
+    this.set('activities', null);
+    this.set('recommendedPosts', null);
+    this._generateRequest(newValue, 'communities');
+  }
+
+  _groupIdChanged(newValue) {
+    this.set('activities', null);
+    this.set('recommendedPosts', null);
+    this._generateRequest(newValue, 'groups');
+  }
+
+  _postIdChanged(newValue) {
+    this.set('activities', null);
+    this.set('recommendedPosts', null);
+    this._generateRequest(newValue, 'posts');
+  }
+
+  _clearScrollThreshold() {
+    this.$$("#scrollTheshold").clearTriggers();
+    console.info("Clearing scrolling triggers for activity");
+  }
+
+  _recommendedPostsResponse(event, detail) {
+    if (detail.response && detail.response.length>0) {
+      this.set('recommendedPosts', detail.response);
+      this.set('noRecommendedPosts', false);
+    } else {
+      this.set('noRecommendedPosts', true);
+    }
+  }
+
+  _preProcessActivities(activities) {
+    for (var i = 0; i < activities.length; i++) {
+      if (activities[i].Point) {
+        activities[i].Point.latestContent = activities[i].Point.PointRevisions[activities[i].Point.PointRevisions.length-1].content;
+      }
+    }
+    return activities;
+  }
+
+  _activitiesResponse(event, detail) {
+    var activities = this._preProcessActivities(detail.response.activities);
+
+    this.set('gotInitialData', true);
+
+    if (detail.response.oldestProcessedActivityAt) {
+      this.set('oldestProcessedActivityAt', detail.response.oldestProcessedActivityAt);
+    } else {
+      console.warn("Have not set oldestProcessedActivityAt");
+    }
+
+    for (var i = 0; i < activities.length; i++) {
+      if (this.$.ajax.url.indexOf('afterDate') > -1) {
+        this.unshift('activities', activities[i]);
+      } else {
+        this.push('activities', activities[i]);
+      }
+    }
+
+    console.info("Activities length: "+activities.length);
+    if (activities.length>0) {
+      if (!this.latestProcessedActivityAt || this.latestProcessedActivityAt < activities[0].created_at) {
+        this.set('latestProcessedActivityAt', activities[0].created_at);
+      }
+      if (!this.latestProcessedActivityAt) {
+        console.error("Have not set latest processed activity at");
+      } else {
+        console.log("latestProcessedActivityAt: "+ this.latestProcessedActivityAt)
+      }
+      this.set('moreToLoad', true);
+      if (this.activities.length<15 || (activities.length<3 && this.activities.length<100)) {
+        this._loadMoreData();
+      }
+    }
+
+    document.dispatchEvent(
+      new CustomEvent("lite-signal", {
+        bubbles: true,
+        compose: true,
+        detail: { name: 'yp-refresh-activities-scroll-threshold', data: {} }
+      })
+    );
+
+    this.async(function () {
+      this.$.ironList.fire('iron-resize');
+    });
+  }
+
+  scrollToItem(item) {
+    console.log("Activity scrolling to item");
+    this.$.ironList.scrollToItem(item);
+    this.async(function () {
+      this._clearScrollThreshold();
+    });
+  }
+
+  fireResize() {
+    console.log("fireResize");
+    this.$.ironList.fire('iron-resize');
+  }
+}
+
+window.customElements.define('ac-activities-lit', AcActivitiesLit)
---
+++ p3_client_app/src/ac-activities/ac-activity-point-news-story-lit.js
@@ -0,0 +1,100 @@
+import '@polymer/polymer/polymer-legacy.js';
+import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
+import { ypLanguageBehavior } from '../yp-behaviors/yp-language-behavior.js';
+import { ypGotoBehavior } from '../yp-behaviors/yp-goto-behavior.js';
+import '../yp-point/yp-point.js';
+import '../yp-point/yp-point-news-story.js';
+import '../yp-magic-text/yp-magic-text.js';
+import { ypActivityGroupTitleBehavior } from './yp-activity-group-title-behavior.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { YpBaseElement } from '../yp-base-element.js';
+
+class AcActivityPointNewsStoryLit extends YpBaseElement {
+  static get properties() {
+    return {
+      activity: {
+        type: Object
+      },
+  
+      postId: {
+        type: Boolean,
+        value: null
+      }
+    }
+  }
+
+  static get styles() {
+    return [
+      css`
+
+      .newsStoryContainer {
+        padding-left: 8px;
+        padding-right: 8px;
+      }
+
+      .postName {
+        cursor: pointer;
+        margin-top: 16px;
+        padding-left: 8px;
+        padding-right: 8px;
+        color: #555;
+      }
+
+      .groupTitle {
+        font-size: 15px;
+        color: #777;
+        padding-bottom: 16px;
+        margin: 0;
+        padding-top: 0;
+      }
+
+      .hasPointer {
+        cursor: pointer;
+      }
+
+      .mainContainer {
+        width: auto;
+      }
+
+      [hidden] {
+        display: none !important;
+      }
+    `, YpFlexLayout]
+  }
+
+  render() {
+    return html`
+    ${this.activity ? html`
+    <div class="layout vertical mainContainer">
+      <yp-magic-text @tap="${this._goToPost}" class="postName" ?hidden="${!this.activity.Post.name}" .textOnly="" .textType="postName" .contentLanguage="${this.activity.Post.language}" .content="${this.activity.Post.name}" .contentId="${this.activity.Post.id}">
+      </yp-magic-text>
+      <div class="layout vertical center-center newsStoryContainer">
+        <yp-point-news-story with-comments ?hideUser class="card" .point="${this.activity.Point}"></yp-point-news-story>
+      </div>
+      <template is="dom-if" if="${this.hasGroupHeader}">
+        <div class="groupTitle layout horizontal center-center">${this.groupTitle}</div>
+      </template>
+    </div>
+` : html``}
+`
+  }
+
+/*
+  behaviors: [
+    ypLanguageBehavior,
+    ypActivityGroupTitleBehavior,
+    ypGotoBehavior
+  ],
+*/
+
+  _goToPost(){
+    this.goToPost(this.activity.Post.id, null, this.activity);
+  }
+
+  _hidePostName(activity) {
+    return (this.postId!=null);
+  }
+}
+
+window.customElements.define('ac-activity-point-news-story-lit', AcActivityPointNewsStoryLit)
