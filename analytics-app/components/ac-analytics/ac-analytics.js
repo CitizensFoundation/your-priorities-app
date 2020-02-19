661721df40cbe9bc6d91b9a4ac5922799d4827a0
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-02-19 11:25:30 +0000

    Remove

--- analytics-app/components/ac-analytics/ac-analytics.js
+++
@@ -1,3 +0,0 @@
-import { AcAnalytics } from './src/AcAnalytics.js';
-
-window.customElements.define('ac-analytics', AcAnalytics);
--- analytics-app/components/ac-analytics/index.js
+++
@@ -1,1 +0,0 @@
-export { AcAnalytics } from './src/AcAnalytics.js';
