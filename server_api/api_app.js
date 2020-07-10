dae6a3217aa3068cddb8324a62831e80457fcd24
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-07-10 16:19:21 +0000

    Merge with master

--- server_api/api_app.js
+++ server_api/api_app.js
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
+require('newrelic');
 
 FORCE_PRODUCTION = false;
 
-const debug = require('debug')('your-priorities-app');
+const log = require('./utils/logger');
+
+if (!process.env.S3_BUCKET) {
+  process.env.S3_BUCKET="no-bucket-implemented-please-set-S3_BUCKET";
+  const errorText = "No S3_BUCKET is set, image uploads and report downloads will not work";
+  log.error(errorText);
+  console.error(errorText);
+}
+
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const ConnectRoles = require('connect-roles');
 const RedisStore = require('connect-redis')(session);
 const useragent = require('express-useragent');
 const requestIp = require('request-ip');
 const compression = require('compression');
+const isBot = require("isbot");
+const redis = require('redis');
 
 const passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy
@@ -41,6 +50,7 @@ const points = require('./controllers/points');
 const users = require('./controllers/users');
 const categories = require('./controllers/categories');
 const images = require('./controllers/images');
+const ratings = require('./controllers/ratings');
 const bulkStatusUpdates = require('./controllers/bulkStatusUpdates');
 const videos = require('./controllers/videos');
 const audios = require('./controllers/audios');
@@ -54,28 +64,41 @@ const nonSPArouter = require('./controllers/nonSpa');
 const generateSitemap = require('./utils/sitemap_generator');
 const generateManifest = require('./utils/manifest_generator');
 
-const log = require('./utils/logger');
 const toJson = require('./utils/to_json');
 const sso = require('passport-sso');
 const cors = require('cors');
+
+const Airbrake = require('@airbrake/node');
+const airbrakeExpress = require('@airbrake/node/dist/instrumentation/express');
+const airbrakePG = require('@airbrake/node/dist/instrumentation/pg');
+
+const ieVersion = (uaString) => {
+  const match = /\b(MSIE |Trident.*?rv:|Edge\/)(\d+)/.exec(uaString);
+  if (match) return parseInt(match[2])
+};
 
 if (process.env.REDISTOGO_URL) {
   process.env.REDIS_URL = process.env.REDISTOGO_URL;
 }
 
-const app = express();
-app.set('port', process.env.PORT || 4242);
-
 let airbrake = null;
 
 if (process.env.AIRBRAKE_PROJECT_ID) {
-  airbrake = require('airbrake').createClient(process.env.AIRBRAKE_PROJECT_ID, process.env.AIRBRAKE_API_KEY);
-  airbrake.handleExceptions();
-  app.use(airbrake.expressHandler());
+  airbrake = new Airbrake.Notifier({
+    projectId: process.env.AIRBRAKE_PROJECT_ID,
+    projectKey: process.env.AIRBRAKE_API_KEY,
+    performanceStats: false
+  });
 }
 
-if (app.get('env') != 'development' && !process.env.DISABLE_FORCE_HTTPS) {
-  app.use(function (req, res, next) {
+const app = express();
+
+if (process.env.AIRBRAKE_PROJECT_ID) {
+  app.use(airbrakeExpress.makeMiddleware(airbrake));
+}
+
+if (app.get('env') !== 'development' && !process.env.DISABLE_FORCE_HTTPS) {
+  app.use(function checkProtocol (req, res, next) {
     if (!/https/.test(req.protocol)) {
       res.redirect("https://" + req.headers.host + req.url);
     } else {
@@ -84,6 +107,19 @@ if (app.get('env') != 'development' && !process.env.DISABLE_FORCE_HTTPS) {
   });
 }
 
+app.use(function checkShortenedRedirects(req, res, next) {
+  if (req.path.startsWith('/s/')) {
+    res.redirect(req.protocol+"://" + req.headers.host + req.url.replace("/s/","/survey/"));
+  } else if (req.path.startsWith('/g/')) {
+    res.redirect(req.protocol+"://" + req.headers.host + req.url.replace("/g/","/group/"));
+  } else if (req.path.startsWith('/c/')) {
+    res.redirect(req.protocol+"://" + req.headers.host + req.url.replace("/c/","/community/"));
+  } else {
+    return next();
+  }
+});
+
+app.set('port', process.env.PORT || 4242);
 app.use(compression());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -118,24 +154,42 @@ app.get('/*', function (req, res, next) {
   next();
 });
 
-if (!FORCE_PRODUCTION && app.get('env') === 'development') {
-  app.use(express.static(path.join(__dirname, '../p3_client_app'), { index: false }));
-} else {
-  app.use(express.static(path.join(__dirname, '../client_app/build/bundled'), { index: false, dotfiles:'allow' }));
-}
+
+app.use(function setupStaticPath(req, res, next) {
+  let staticPath = path.join(__dirname, '../client_app/build/bundled');
+  let staticIndex = false;
+
+  if (req.path.startsWith('/marketing/') || (req.headers.referrer && req.headers.referrer.indexOf('/marketing/')>-1)) {
+    staticPath = path.join(__dirname, '../marketing_app/dist');
+    staticIndex = "index.html";
+  } else if (req.path.startsWith('/analytics/') || (req.headers.referrer && req.headers.referrer.indexOf('/analytics/'))>-1) {
+    staticPath = path.join(__dirname, '../analytics_app/dist');
+    staticIndex = "index.html";
+  } else {
+    if (!FORCE_PRODUCTION && app.get('env') === 'development') {
+      staticPath = path.join(__dirname, '../client_app');
+    }
+  }
+
+  express.static(staticPath, { index: staticIndex, dotfiles:'allow' })(req,res,next);
+});
+
 
 app.use(session(sessionConfig));
 
 // Setup the current domain from the host
-app.use(function (req, res, next) {
+app.use(function setupDomain(req, res, next) {
   models.Domain.setYpDomain(req, res, function () {
-    log.info("Setup Domain Completed", {context: 'setYpDomain', domain: req.ypDomain ? toJson(req.ypDomain.simple()) : null});
+    log.info("Setup Domain Completed", {context: 'setYpDomain',
+      domainId: req.ypDomain ? req.ypDomain.id : null,
+      domainName: req.ypDomain ? req.ypDomain.domain_name : null
+    });
     next();
   });
 });
 
 // Setup the current community from the host
-app.use(function (req, res, next) {
+app.use(function setupCommunity(req, res, next) {
   models.Community.setYpCommunity(req, res, function () {
     log.info("Setup Community Completed", {context: 'setYpCommunity', community: req.ypCommunity.hostname});
     next();
@@ -145,21 +199,38 @@ app.use(function (req, res, next) {
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(function (req, res, next) {
+app.use(function setupRedis(req, res, next) {
+  req.redisClient = sessionConfig.store.client;
+  next();
+});
+
+app.get('/sitemap.xml', function getSitemap(req, res) {
+  const redisKey = "cache:sitemapv14:" + req.ypDomain.id + (req.ypCommunity && req.ypCommunity.id && req.ypCommunity.hostname) ? req.ypCommunity.hostname : '';
+  req.redisClient.get(redisKey, (error, sitemap) => {
+    if (error) {
+      log.error("Error getting sitemap from redis", {error});
+      generateSitemap(req, res);
+    } else if (sitemap) {
+      res.header('Content-Type', 'application/xml');
+      res.set({ 'content-type': 'application/xml' });
+      res.send(sitemap);
+    } else {
+      generateSitemap(req, res);
+    }
+  });
+});
+
+app.use(function checkForBOT(req, res, next) {
   var ua = req.headers['user-agent'];
-  if (/^(facebookexternalhit)|(web\/snippet)|(Twitterbot)|(Slackbot)|(Embedly)|(LinkedInBot)|(Pinterest)|(XING-contenttabreceiver)/gi.test(ua)) {
-    console.log(ua, ' is a bot');
+  if (!/Googlebot|AdsBot-Google/.test(ua) && (isBot(ua) || /^(facebookexternalhit)|(web\/snippet)|(Twitterbot)|(Slackbot)|(Embedly)|(LinkedInBot)|(Pinterest)|(XING-contenttabreceiver)/gi.test(ua))) {
+    log.info('Request is from a bot', { ua });
     nonSPArouter(req, res, next);
   } else {
     next();
   }
 });
 
-app.get('/sitemap.xml', function (req, res) {
-  generateSitemap(req, res);
-});
-
-app.get('/manifest.json', function (req, res) {
+app.get('/manifest.json', function getManifest(req, res) {
   generateManifest(req, res);
 });
 
@@ -167,7 +238,7 @@ var bearerCallback = function (req, token) {
   return console.log('The user has tried to authenticate with a bearer token');
 };
 
-app.use(function (req, res, next) {
+app.use(function checkAuthForSsoInit(req, res, next) {
   if (req.url.indexOf('/auth') > -1 || req.url.indexOf('/login') > -1 || req.url.indexOf('saml_assertion') > -1) {
     sso.init(req.ypDomain.loginHosts, req.ypDomain.loginProviders, {
       authorize: bearerCallback,
@@ -207,7 +278,7 @@ const registerUserLogin = (user, userId, loginProvider, req, done) => {
   if (user && user.private_profile_data) {
     completeRegisterUserLogin(user, loginProvider, req, done);
   } else {
-    models.User.find({
+    models.User.findOne({
       where: {
         id: userId
       },
@@ -226,7 +297,7 @@ const registerUserLogin = (user, userId, loginProvider, req, done) => {
   }
 };
 
-passport.serializeUser(function (req, profile, done) {
+passport.serializeUser(function userSerialize(req, profile, done) {
   log.info("User Serialized", { logionProvider: profile.provider });
   if (profile.provider && profile.provider === 'facebook') {
     models.User.serializeFacebookUser(profile, req.ypDomain, function (error, user) {
@@ -264,9 +335,9 @@ passport.serializeUser(function (req, profile, done) {
   }
 });
 
-passport.deserializeUser(function (sessionUser, done) {
+passport.deserializeUser(function deserializeUser(sessionUser, done) {
   log.info("Debug passport.deserializeUser", { sessionUser });
-  models.User.find({
+  models.User.findOne({
     where: {id: sessionUser.userId},
     attributes: ["id", "name", "email", "default_locale", "facebook_id", "twitter_id", "google_id", "github_id", "ssn", "profile_data", 'private_profile_data'],
     include: [
@@ -292,12 +363,12 @@ passport.deserializeUser(function (sessionUser, done) {
     } else {
       log.error("User Deserialized Not found", {context: 'deserializeUser'});
       if (airbrake) {
-        airbrake.notify("User Deserialized Not found", function (airbrakeErr, url) {
-          if (airbrakeErr) {
+        airbrake.notify("User Deserialized Not found").then((airbrakeErr) => {
+          if (airbrakeErr.error) {
             log.error("AirBrake Error", {
               context: 'airbrake',
               user: toJson(req.user),
-              err: airbrakeErr,
+              err: airbrakeErr.error,
               errorStatus: 500
             });
           }
@@ -308,12 +379,12 @@ passport.deserializeUser(function (sessionUser, done) {
   }).catch(function (error) {
     log.error("User Deserialize Error", {context: 'deserializeUser', user: sessionUser.userId, err: error, errorStatus: 500});
     if (airbrake) {
-      airbrake.notify(error, function (airbrakeErr, url) {
-        if (airbrakeErr) {
+      airbrake.notify(error).then((airbrakeErr) => {
+        if (airbrakeErr.error) {
           log.error("AirBrake Error", {
             context: 'airbrake',
             user: null,
-            err: airbrakeErr,
+            err: airbrakeErr.error,
             errorStatus: 500
           });
         }
@@ -326,7 +397,7 @@ passport.deserializeUser(function (sessionUser, done) {
 app.use('/', index);
 
 // Set caching for IE so it wont cache the json queries
-app.use(function (req, res, next) {
+app.use(function cacheControlHeaders(req, res, next) {
   var ua = req.headers['user-agent'];
   if (/Trident/gi.test(ua)) {
     res.set("Cache-Control", "no-cache,no-store");
@@ -334,11 +405,17 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.use('/marketing', express.static(path.join(__dirname, '../marketing_app/dist')));
+app.use('/analytics/', express.static(path.join(__dirname, '../analytics_app/dist')));
+app.use('/analytics/domain/*', express.static(path.join(__dirname, '../analytics_app/dist')));
+app.use('/analytics/community/*', express.static(path.join(__dirname, '../analytics_app/dist')));
+app.use('/analytics/group/*', express.static(path.join(__dirname, '../analytics_app/dist')));
 app.use('/domain', index);
 app.use('/community', index);
 app.use('/group', index);
 app.use('/post', index);
 app.use('/user', index);
+app.use('/survey*', index);
 app.use('/api/domains', domains);
 app.use('/api/organizations', organizations);
 app.use('/api/communities', communities);
@@ -355,6 +432,7 @@ app.use('/api/activities', activities);
 app.use('/api/notifications', notifications);
 app.use('/api/bulk_status_updates', bulkStatusUpdates);
 app.use('/api/recommendations', recommendations);
+app.use('/api/ratings', ratings);
 app.use('/ideas', legacyPosts);
 app.use('/users', legacyUsers);
 app.use('/pages', legacyPages);
@@ -367,9 +445,9 @@ app.post('/authenticate_from_island_is', function (req, res) {
       log.error("Error from SAML login", {err: error});
       error.url = req.url;
       if (airbrake) {
-        airbrake.notify(error, function (airbrakeErr, url) {
-          if (airbrakeErr) {
-            log.error("AirBrake Error", {context: 'airbrake', err: airbrakeErr, errorStatus: 500});
+        airbrake.notify(error).then((airbrakeErr) => {
+          if (airbrakeErr.error) {
+            log.error("AirBrake Error", {context: 'airbrake', err: airbrakeErr.error, errorStatus: 500});
           }
           res.sendStatus(500);
         });
@@ -395,9 +473,9 @@ app.post('/saml_assertion', function (req, res) {
       } else {
         error.url = req.url;
         if (airbrake) {
-          airbrake.notify(error, function (airbrakeErr, url) {
-            if (airbrakeErr) {
-              log.error("AirBrake Error", {context: 'airbrake', err: airbrakeErr, errorStatus: 500});
+          airbrake.notify(error).then((airbrakeErr) => {
+            if (airbrakeErr.error) {
+              log.error("AirBrake Error", {context: 'airbrake', err: airbrakeErr.error, errorStatus: 500});
             }
             res.sendStatus(500);
           });
@@ -412,9 +490,9 @@ app.post('/saml_assertion', function (req, res) {
   })
 });
 
-app.use(function (err, req, res, next) {
+app.use(function check401errors(err, req, res, next) {
   if (err instanceof auth.UnauthorizedError) {
-    log.info("Anon debug UnauthorizedError", { user: req && req.user ? req.user : null});
+    log.info("Anon debug UnauthorizedError", { user: req.user });
     log.error("User Unauthorized", {
       context: 'unauthorizedError',
       user: toJson(req.user),
@@ -428,7 +506,7 @@ app.use(function (err, req, res, next) {
 });
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function check4040errors(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   log.warn("Not Found", {context: 'notFound', user: toJson(req.user), err: 'Not Found', errorStatus: 404});
@@ -436,14 +514,14 @@ app.use(function (req, res, next) {
 });
 
 // Error handlers
-app.use(function (err, req, res, next) {
+app.use(function generalErrorHandler(err, req, res, next) {
   var status = err.status || 500;
   if (status == 404) {
     log.warn("Not found", {context: 'notFound', errorStatus: status, url: req.url});
   } else {
     log.error("General Error", {
       context: 'generalError',
-      user: req ? toJson(req.user) : null,
+      user: req.user ? toJson(req.user) : null,
       err: err,
       errStack: err.stack,
       errorStatus: status
@@ -453,33 +531,28 @@ app.use(function (err, req, res, next) {
   err.params = req.params;
   if (status != 404 && status != 401) {
     if (airbrake) {
-      airbrake.notify(err, function (airbrakeErr, url) {
-        if (airbrakeErr) {
+      airbrake.notify(err).then((airbrakeErr)=> {
+        if (airbrakeErr.error) {
           log.error("AirBrake Error", {
             context: 'airbrake',
             user: toJson(req.user),
-            err: airbrakeErr,
+            err: airbrakeErr.error,
             errorStatus: 500
           });
         }
-        res.status(status).send({
-          message: err.message,
-          error: err
-        });
+        res.sendStatus(status);
       });
     } else {
-      res.status(status).send({
-        message: err.message,
-        error: err
-      });
+      res.sendStatus(status);
     }
   } else {
-    res.status(status).send({
-      message: err.message,
-      error: err
-    });
+    res.sendStatus(status);
   }
 });
+
+if (airbrake) {
+  app.use(airbrakeExpress.makeErrorHandler(airbrake));
+}
 
 var server = app.listen(app.get('port'), function () {
   log.info('Your Priorities server listening on port ' + server.address().port);
