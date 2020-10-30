b00dba41f1380d13875005387b6b9b9ede97c650
Alexander <alexander.gautason@gmail.com>
2020-10-30 14:22:53 +0000

    testa post

--- apps/client_app/src/yp-post/test/yp-post-actions.test.ts
+++ apps/client_app/src/yp-post/test/yp-post-actions.test.ts
@@ -15,7 +15,29 @@ describe('YpPostActions', () => {
 
   beforeEach(async () => {
     const post = {
-    } as YpPointData;
+      id: 1,
+      location:{
+        latitude: 2,
+        longitude: 3,
+      },
+      name: 'Robert',
+      group_id: 1,
+      description: 'Post-Test',
+      counter_endorsements_up: 2,
+      counter_endorsements_down: 4,
+      counter_points: 5,
+      Group: {
+        id: 1,
+        name: 'Alex',
+        community_id: 1,
+        counter_points: 1,
+        counter_users: 2,
+        counter_posts: 1,
+        configuration: {
+          makeMapViewDefault: false
+        }
+      }
+    } as YpPostData;
 
 
     element = await fixture(html`
