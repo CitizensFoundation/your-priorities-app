d2e8f7f83205d3434b70a769df17524180ad5908
Alexander <alexander.gautason@gmail.com>
2020-10-30 16:03:00 +0000

    finnished post and dialog

---
+++ apps/client_app/src/yp-page/test/yp-page-dialog.test.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { html, fixture, expect } from '@open-wc/testing';
+
+import { YpPageDialog } from '../yp-page-dialog.js';
+import '../yp-page-dialog.js';
+import { YpTestHelpers } from '../../common/test/setup-app.js';
+
+describe('YpPageDialog', () => {
+  let element: YpPageDialog;
+
+  before(async () => {
+    await YpTestHelpers.setupApp();
+  });
+
+  beforeEach(async () => {
+    const page  = {
+    id: 1,
+    content: {"en":"Blah"},
+    title: {"en":"blur"},
+    } as YpHelpPage;
+
+
+    element = await fixture(html`
+      <yp-page-dialog
+      .page="${page}">
+      </yp-page-dialog>
+    `);
+  });
+  
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
