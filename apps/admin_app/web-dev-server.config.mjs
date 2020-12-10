31fb38609c874e0541723643b6b9868a1c939f39
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-12-10 22:41:38 +0000

    Upgrade to modern web

---
+++ apps/admin_app/web-dev-server.config.mjs
@@ -0,0 +1,27 @@
+import { fromRollup } from '@web/dev-server-rollup';
+import rollupCommonjs from '@rollup/plugin-commonjs';
+import proxy from 'koa-proxies';
+
+const commonjs = fromRollup(rollupCommonjs);
+
+export default {
+  mimeTypes: {
+    '**/*.cjs': 'js'
+  },
+  port: 9000,
+  middleware: [
+    proxy('/api/', {
+      target: 'http://localhost:4242/',
+    }),
+  ],
+  plugins: [
+    commonjs({
+      include: [
+        'node_modules/linkifyjs/**/*',
+        'node_modules/moment/**/*',
+        '**/*/node_modules/linkifyjs/**/*',
+        '**/*/node_modules/i18next-http-backend/**/*'
+      ],
+    }),
+  ],
+};
