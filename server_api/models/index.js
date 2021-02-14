2b7a061b33ca755555888a5f92ff6930a89d1e7c
Robert Bjarnason <robert.bjarnason@gmail.com>
2021-02-14 14:14:57 +0000

    Work on postgres upgrade

--- server_api/models/index.js
+++ server_api/models/index.js
@@ -29,11 +29,12 @@ const operatorsAliases = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize(process.env.DATABASE_URL, null, null, {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
-      ssl: true,
-      rejectUnauthorized: false
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
     minifyAliases: true,
     logging: false,
