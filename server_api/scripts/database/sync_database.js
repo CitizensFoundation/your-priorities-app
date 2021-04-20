809cb8c5a943377ee2c279815cf31617aba7170d
Robert Bjarnason <robert.bjarnason@gmail.com>
2021-04-20 16:45:25 +0000

    Work on db

--- server_api/scripts/database/sync_database.js
+++ server_api/scripts/database/sync_database.js
@@ -2,4 +2,11 @@ const models = require('../../models');
 
 models.sequelize.sync().then(() => {
   models.Post.addFullTextIndex();
+  setTimeout(()=>{
+    console.log("Time has passed");
+    process.exit();
+  }, 15000)
+}).catch( error=>{
+  console.error(error);
+  process.exit();
 });
