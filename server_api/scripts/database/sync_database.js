e471681ec974ee053da9d61221c9c1abd19147a1
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-04-20 19:33:31 +0000

    Dev setup

--- server_api/scripts/database/sync_database.js
+++ server_api/scripts/database/sync_database.js
@@ -1,19 +1,17 @@
 const models = require('../../models');
 
-
-models.sequelize.query('CREATE DATABASE yrpri_dev', (err, res) => {
-  console.log(err, res);
-  models.sequelize.sync({}).then(() => {
+models.sequelize.sync({}).then(() => {
+  setTimeout(()=>{
+    models.Post.addFullTextIndex();
     setTimeout(()=>{
-      models.Post.addFullTextIndex();
-      setTimeout(()=>{
-        console.log("Time has passed");
-        process.exit();
-      }, 15000)
-    }, 1000)
-  }).catch( error=>{
-    console.error(error);
-    process.exit();
-  });
+      console.log("Time has passed");
+      process.exit();
+    }, 15000)
+  }, 1000)
+
+}).catch( error=>{
+  console.error(error);
+  process.exit();
 });
+
 
--- server_api/scripts/database/sync_dev_database.js
+++ server_api/scripts/database/sync_dev_database.js
@@ -1,15 +1,17 @@
 const models = require('../../models');
 
-models.sequelize.sync({}).then(() => {
-  setTimeout(()=>{
-    models.Post.addFullTextIndex();
+models.sequelize.query('CREATE DATABASE yrpri_dev', (err, res) => {
+  console.log(err, res);
+  models.sequelize.sync({}).then(() => {
     setTimeout(()=>{
-      console.log("Time has passed");
-      process.exit();
-    }, 15000)
-  }, 1000)
-
-}).catch( error=>{
-  console.error(error);
-  process.exit();
+      models.Post.addFullTextIndex();
+      setTimeout(()=>{
+        console.log("Time has passed");
+        process.exit();
+      }, 15000)
+    }, 1000)
+  }).catch( error=>{
+    console.error(error);
+    process.exit();
+  });
 });
