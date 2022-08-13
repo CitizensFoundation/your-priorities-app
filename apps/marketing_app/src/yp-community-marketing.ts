6e73a87393377ea0f4e5f759641886524de8dd45
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-08-13 06:15:24 +0000

    Merge in Plausible backend support

---
+++ apps/marketing_app/src/yp-community-marketing.ts
@@ -0,0 +1,92 @@
+import { LitElement, css, html, nothing } from 'lit';
+import { property, customElement } from 'lit/decorators.js';
+import 'chart.js';
+
+import { YpAdminPage } from './yp-admin-page.js';
+
+import { Chart, registerables } from 'chart.js';
+
+import './yp-visitors-chart.js';
+import './yp-pages-chart.js';
+import './yp-bounce-rate-chart.js';
+
+@customElement('yp-community-marketing')
+export class YpCommunityMarketing extends YpAdminPage {
+  @property({ type: String })
+  communityAccess = 'public';
+
+  chart: any;
+
+  constructor() {
+    super();
+    Chart.register(...registerables);
+  }
+
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        .header {
+          font-size: 18px;
+          font-weight: bold;
+          text-align: center;
+          margin-top: 16px;
+          margin-bottom: 16px;
+        }
+
+        .nextHeader {
+          margin-top: 32px;
+        }
+      `,
+    ];
+  }
+
+  render() {
+    return this.collection
+      ? html`
+          <div class="layout horizontal wrap">
+            <div class="layout vertical">
+              ${this.collection
+                ? html`
+                    <div class="header">Visitors "day" Chart</div>
+                    <yp-visitors-chart
+                      .collection="${this.collection}"
+                      .collectionId="${this.collectionId}"
+                    ></yp-visitors-chart>
+
+                    <div class="header nextHeader">Pages "day" Chart</div>
+                    <yp-pages-chart
+                      .collection="${this.collection}"
+                      .collectionId="${this.collectionId}"
+                    ></yp-pages-chart>
+
+                    <div class="header nextHeader">Bounce rate Chart</div>
+                    <yp-bounce-rate-chart
+                      .collection="${this.collection}"
+                      .collectionId="${this.collectionId}"
+                    ></yp-bounce-rate-chart>
+
+                  `
+                : nothing}
+            </div>
+          </div>
+        `
+      : nothing;
+  }
+
+  updated(changedProperties: Map<string | number | symbol, unknown>): void {
+    super.updated(changedProperties);
+
+    if (changedProperties.has('collection') && this.collection) {
+      this._communityChanged();
+    }
+
+    if (changedProperties.has('collectionId') && this.collectionId) {
+      this._collectionIdChanged();
+    }
+  }
+
+  async _communityChanged() {}
+
+  _collectionIdChanged() {}
+}
