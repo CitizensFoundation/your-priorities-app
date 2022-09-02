9dba94a79a7436de985bcc644c528ab504b83deb
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-09-02 21:20:07 +0000

    Better og and twitter cards

---
+++ server_api/utils/sharing_parameters.js
@@ -0,0 +1,51 @@
+const models = require('../models');
+
+const updateParametersFromCampaign = async (utmContent, parameters) => {
+  return await new Promise(async (resolve, reject) => {
+    try {
+      const campaign = await models.Campaign.findOne({
+        where: {
+          id: utmContent
+        },
+        attributes: ['id','configuration']
+      });
+
+      if (campaign &&
+          campaign.configuration.shareImage) {
+        parameters.image = campaign.configuration.shareImage;
+      }
+
+      if (campaign &&
+        campaign.configuration.shareImage) {
+        parameters.description = campaign.configuration.shareText;
+      }
+
+      resolve(parameters);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+const getSharingParameters = async (req, collection, url, imageUrl) => {
+  return await new Promise(async (resolve, reject) => {
+    let parameters = {
+      title: collection.title,
+      description: collection.description || collection.objectives,
+      url: url,
+      image: imageUrl
+    }
+    try {
+      if (req.query.utm_content && !isNaN(req.query.utm_content))  {
+        parameters = await updateParametersFromCampaign(req.query.utm_content, parameters)
+      }
+      resolve(parameters);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+module.exports = {
+  getSharingParameters
+};
