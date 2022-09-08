e0989de2706791be8e211168915d6e956975fcc3
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-09-08 01:02:13 +0000

    Optimize sitemaps

--- server_api/utils/sitemap_generator.js
+++ server_api/utils/sitemap_generator.js
@@ -2,6 +2,7 @@ var models = require("../models");
 var _ = require("lodash");
 var sitemapLib = require('sitemap');
 var async = require('async');
+const log = require('./logger');
 
 const getCommunityURL = (hostname, domainName, path) => {
   return 'https://'+hostname+'.'+domainName+path;
@@ -164,6 +165,7 @@ var generateSitemap = function(req, res) {
                   where: {
                     id: domainId
                   },
+                  attributes: [],
                   required: true
                 }
               ]
