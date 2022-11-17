a7d8c2bafadd20bd9a909cd6eb4eb5693551bc33
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-11-17 11:31:53 +0000

    Restore correct new admin app

---
+++ apps/admin_app/src/yp-bulk-status-update/test/yp-bulk-status-update-templates.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { html, fixture, expect } from '@open-wc/testing';
+
+import { YpBulkStatusUpdateTemplates } from '../yp-bulk-status-update-templates.js';
+import '../yp-bulk-status-update-templates.js';
+import { YpTestHelpers } from '../../@yrpri/common/test/setup-app.js';
+
+
+describe('YpBulkStatusUpdateTemplates', () => {
+  let element: YpBulkStatusUpdateTemplates;
+
+  before(async () => {
+    await YpTestHelpers.setupApp();
+  });
+
+  beforeEach(async () => {
+
+    element = await fixture(html`
+      <yp-bulk-status-templates
+        headerText="BLAHHA"
+      ></yp-bulk-status-templates>
+    `);
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
