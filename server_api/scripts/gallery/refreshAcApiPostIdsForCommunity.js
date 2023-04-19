1eeff5def775d8eb923a86036d767acf7aeccc81
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-04-19 19:10:01 +0000

    Work on gallery features

---
+++ server_api/scripts/gallery/refreshAcApiPostIdsForCommunity.js
@@ -0,0 +1,61 @@
+const queue = require('../../active-citizen/workers/queue');
+const models = require("../../models");
+
+(async () => {
+  console.log("Start")
+  let offset = 0;
+  let count= 0;
+  let continueFetching = true;
+  const chunkSize= 100;
+  const communityId = process.argv[2];
+  const postsProcessed = {};
+
+  while (continueFetching) {
+    console.log(`Fetching posts ${offset} to ${offset + chunkSize - 1}...`)
+    const posts = await models.Post.findAll({
+      attributes: [
+        "id","name"
+      ],
+      include: [
+        {
+          model: models.Group,
+          attributes: ['id'],
+          include: [
+            {
+              model: models.Community,
+              attributes: ['id'],
+              where: {
+                id: process.argv[2]
+              }
+            }
+          ]
+        }
+      ],
+      limit: chunkSize,
+      offset: offset
+    });
+
+    // Process the posts
+    for (let p=0; p<posts.length; p++) {
+      console.log(`${posts[p].id} ${posts[p].name}`);
+      queue.add('process-similarities', { type: 'update-collection', postId: posts[p].id }, 'low');
+      await new Promise(resolve => setTimeout(resolve, 150));
+      count++;
+      if (postsProcessed[posts[p].id]) {
+        console.error(`Post ${posts[p].id} already processed!`);
+      } else {
+        postsProcessed[posts[p].id] = true;
+      }
+    }
+
+    // Check if there are fewer posts than the chunk size, which indicates that it's the last chunk
+    if (posts.length < chunkSize) {
+      continueFetching = false;
+    } else {
+      offset += chunkSize;
+    }
+  }
+
+  console.log(`Done. Processed ${count} posts.`)
+})();
+
