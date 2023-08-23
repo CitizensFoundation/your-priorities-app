37bec58b5d3e58e5a54c5045396cbf0df611d993
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-08-23 21:15:52 +0000

    Work on land use game

--- apps/land_use_game/src/@yrpri/yp-point/yp-point-comment.ts
+++ apps/land_use_game/src/@yrpri/yp-point/yp-point-comment.ts
@@ -1,20 +1,20 @@
-import { html, css, nothing } from 'lit';
-import { property, customElement } from 'lit/decorators.js';
+import { html, css, nothing } from "lit";
+import { property, customElement } from "lit/decorators.js";
 
-import '@material/mwc-icon-button';
-import '@material/web/iconbutton/icon-button.js';
+import "@material/mwc-icon-button";
+import "@material/web/iconbutton/icon-button.js";
 
-import '../yp-user/yp-user-image.js';
-import '../yp-user/yp-user-with-organization.js';
-import './yp-point-actions.js';
+import "../yp-user/yp-user-image.js";
+import "../yp-user/yp-user-with-organization.js";
+import "./yp-point-actions.js";
 
-import { YpAccessHelpers } from '../common/YpAccessHelpers.js';
-import { YpBaseElementWithLogin } from '../common/yp-base-element-with-login.js';
-import { YpConfirmationDialog } from '../yp-dialog-container/yp-confirmation-dialog.js';
-import { YpApiActionDialog } from '../yp-api-action-dialog/yp-api-action-dialog.js';
-import { Layouts } from '../../flexbox-literals/classes.js';
+import { YpAccessHelpers } from "../common/YpAccessHelpers.js";
+import { YpBaseElementWithLogin } from "../common/yp-base-element-with-login.js";
+import { YpConfirmationDialog } from "../yp-dialog-container/yp-confirmation-dialog.js";
+import { YpApiActionDialog } from "../yp-api-action-dialog/yp-api-action-dialog.js";
+import { Layouts } from "../../flexbox-literals/classes.js";
 
-@customElement('yp-point-comment')
+@customElement("yp-point-comment")
 export class YpPointComment extends YpBaseElementWithLogin {
   @property({ type: Object })
   point: YpPointData | undefined;
@@ -28,7 +28,7 @@ export class YpPointComment extends YpBaseElementWithLogin {
   updated(changedProperties: Map<string | number | symbol, unknown>): void {
     super.updated(changedProperties);
 
-    if (changedProperties.has('point')) {
+    if (changedProperties.has("point")) {
       this._pointChanged();
     }
   }
@@ -38,7 +38,6 @@ export class YpPointComment extends YpBaseElementWithLogin {
       Layouts,
       css`
         .userName {
-
         }
 
         .extraActions {
@@ -73,7 +72,6 @@ export class YpPointComment extends YpBaseElementWithLogin {
         }
 
         #reportPointIconButton {
-
           width: 36px;
           height: 36px;
         }
@@ -83,95 +81,119 @@ export class YpPointComment extends YpBaseElementWithLogin {
         }
 
         #deleteButton {
-
         }
       `,
     ];
   }
 
   render() {
-    return this.point ? html`
-      <div class="layout horizontal">
-        <div class="layout horizontal">
-          <yp-user-with-organization .user="${this.user}"></yp-user-with-organization>
-        </div>
-        <div class="layout vertical">
-          <div class="comment">
-            ${this.point.content}
+    return this.point
+      ? html`
+          <div class="layout ${this.wide ? "horizontal" : "vertical"}">
             <div class="layout horizontal">
-              <yp-point-actions
-                .point="${this.point}"
-                hideSharing></yp-point-actions>
-              <div class="extraActions layout horizontal">
-              <md-icon-button
-                ?hisdden=""
-                ?hidsddden="${!this.loggedInUser}"
-                hidden
-                .label="${this.t('point.report')}"
-                id="reportPointIconButton"
-                @click="${this._reportPoint}"><md-icon>warning</md-icon></md-icon-button>
+              <yp-user-with-organization
+                .user="${this.user}"
+              ></yp-user-with-organization>
+            </div>
+            <div class="layout vertical">
+              <div class="comment">
+                ${this.point.content}
+                <div class="layout horizontal">
+                  <yp-point-actions
+                    .point="${this.point}"
+                    hideSharing
+                  ></yp-point-actions>
+                  <div class="extraActions layout horizontal">
+                    <md-icon-button
+                      ?hisdden=""
+                      ?hidsddden="${!this.loggedInUser}"
+                      hidden
+                      .label="${this.t("point.report")}"
+                      id="reportPointIconButton"
+                      @click="${this._reportPoint}"
+                      ><md-icon>warning</md-icon></md-icon-button
+                    >
 
-              ${this.hasPointAccess
-                ? html`
-                    <div class="layout horizontal self-end">
-                      <md-icon-button
-                        id="deleteButton"
-                        .label="${this.t('delete')}"
-                        @click="${this._deletePoint}"><md-icon>clear</md-icon></md-icon-button>
-                    </div>
-                  `
-                : html``}
-
+                    ${this.hasPointAccess
+                      ? html`
+                          <div class="layout horizontal self-end">
+                            <md-icon-button
+                              id="deleteButton"
+                              .label="${this.t("delete")}"
+                              @click="${this._deletePoint}"
+                              ><md-icon>clear</md-icon></md-icon-button
+                            >
+                          </div>
+                        `
+                      : html``}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    ` : nothing
+        `
+      : nothing;
   }
 
   connectedCallback() {
-    super.connectedCallback()
-    this.addGlobalListener('yp-logged-in', this.requestUpdate.bind(this))
-    this.addGlobalListener('yp-got-admin-rights', this.requestUpdate.bind(this))
+    super.connectedCallback();
+    this.addGlobalListener("yp-logged-in", this.requestUpdate.bind(this));
+    this.addGlobalListener(
+      "yp-got-admin-rights",
+      this.requestUpdate.bind(this)
+    );
   }
 
   disconnectedCallback() {
-    super.disconnectedCallback()
-    this.removeGlobalListener('yp-logged-in', this.requestUpdate.bind(this))
-    this.removeGlobalListener('yp-got-admin-rights', this.requestUpdate.bind(this))
+    super.disconnectedCallback();
+    this.removeGlobalListener("yp-logged-in", this.requestUpdate.bind(this));
+    this.removeGlobalListener(
+      "yp-got-admin-rights",
+      this.requestUpdate.bind(this)
+    );
   }
 
   _deletePoint() {
-    window.appDialogs.getDialogAsync("confirmationDialog", (dialog: YpConfirmationDialog) => {
-      dialog.open(this.t('point.confirmDelete'), this._reallyDeletePoint.bind(this));
-    });
+    window.appDialogs.getDialogAsync(
+      "confirmationDialog",
+      (dialog: YpConfirmationDialog) => {
+        dialog.open(
+          this.t("point.confirmDelete"),
+          this._reallyDeletePoint.bind(this)
+        );
+      }
+    );
   }
 
   async _reallyDeletePoint() {
     if (this.point) {
-      await window.serverApi.deletePoint(this.point.id)
-      this.fire('yp-point-deleted', { pointId: this.point!.id });
-      this.fire('iron-resize');
+      await window.serverApi.deletePoint(this.point.id);
+      this.fire("yp-point-deleted", { pointId: this.point!.id });
+      this.fire("iron-resize");
       this.point = undefined;
     }
   }
 
   _reportPoint() {
-    window.appGlobals.activity('open', 'point.report');
-    window.appDialogs.getDialogAsync("apiActionDialog", (dialog: YpApiActionDialog) => {
-      dialog.setup('/api/points/' + this.point!.id + '/report',
-        this.t('reportConfirmation'),
-        this._onReport.bind(this),
-        this.t('point.report'),
-        'PUT');
-      dialog.open();
-    });
+    window.appGlobals.activity("open", "point.report");
+    window.appDialogs.getDialogAsync(
+      "apiActionDialog",
+      (dialog: YpApiActionDialog) => {
+        dialog.setup(
+          "/api/points/" + this.point!.id + "/report",
+          this.t("reportConfirmation"),
+          this._onReport.bind(this),
+          this.t("point.report"),
+          "PUT"
+        );
+        dialog.open();
+      }
+    );
   }
 
   _onReport() {
     window.appGlobals.notifyUserViaToast(
-      this.t('point.report') + ': ' + this.point!.content
+      this.t("point.report") + ": " + this.point!.content
     );
   }
 
