215241c523f18d0f3bfb40429b26d85f15381ca4
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-11-30 03:10:59 +0000

    Work on invite code

--- server_api/scripts/createInvitesAndShow.js
+++ server_api/scripts/createInvitesAndShow.js
@@ -1,5 +1,4 @@
 const models = require('../models');
-const async = require('async');
 const ip = require('ip');
 const _ = require('lodash');
 const toJson = require('../utils/to_json');
@@ -10,129 +9,76 @@ const senderUserId = process.argv[3];
 const numberOfInvites = process.argv[4];
 const masterCampaignName = process.argv[5];
 
-const createOneInvite = (campaignName, inviteEmail, callback) => {
+const createOneInvite = async (campaignName, inviteEmail) => {
   let community, invite, registeredUser, senderUser, token, inviteUrl;
 
-  async.series([
-    (seriesCallback) => {
-      crypto.randomBytes(20, function(error, buf) {
-        token = buf.toString('hex');
-        seriesCallback(error);
+  try {
+    token = (await crypto.randomBytes(20)).toString('hex');
+
+    community = await models.Community.findOne({
+      where: { id: communityId },
+      attributes: ['id', 'domain_id', 'hostname'],
+      include: [{ model: models.Domain, attributes: ['id', 'domain_name'] }]
+    });
+
+    senderUser = await models.User.findOne({
+      where: { id: senderUserId },
+      attributes: ['id', 'email', 'name']
+    });
+
+    if (inviteEmail) {
+      registeredUser = await models.User.findOne({
+        where: { email: inviteEmail },
+        attributes: ['id', 'email']
       });
-    },
-    (seriesCallback) => {
-      models.Community.findOne({
-        where: {
-          id: communityId
-        },
-        attributes: ['id', 'domain_id','hostname'],
-        include: [
-          {
-            model: models.Domain,
-            attributes: ['id','domain_name']
-          }
-        ]
-      }).then((communityIn) => {
-        community = communityIn;
-        seriesCallback();
-      }).catch((error) => {
-        seriesCallback(error);
-      })
-    },
-    (seriesCallback) => {
-      models.User.findOne({
-        where: { id: senderUserId },
-        attributes: ['id','email']
-      }).then(function (userIn) {
-        if (userIn) {
-          senderUser = userIn;
-        }
-        seriesCallback();
-      }).catch(function (error) {
-        seriesCallback(error);
-      });
-    },
-    (seriesCallback) => {
-      if (inviteEmail) {
-        models.User.findOne({
-          where: { email: inviteEmail },
-          attributes: ['id','email']
-        }).then(function (userIn) {
-          if (userIn) {
-            registeredUser = userIn;
-          }
-          seriesCallback();
-        }).catch(function (error) {
-          seriesCallback(error);
-        });
-      } else {
-        seriesCallback();
-      }
-    },
-    (seriesCallback) => {
-      models.Invite.create({
-        token: token,
-        expires_at: Date.now() + (3600000*24*30),
-        type: models.Invite.INVITE_TO_COMMUNITY,
-        community_id: communityId,
+    }
+
+    invite = await models.Invite.create({
+      token: token,
+      expires_at: Date.now() + (3600000 * 24 * 30),
+      type: models.Invite.INVITE_TO_COMMUNITY,
+      community_id: communityId,
+      user_id: registeredUser ? registeredUser.id : null,
+      from_user_id: senderUser.id,
+      metadata: { toEmail: inviteEmail, campaignName: campaignName }
+    });
+
+    if (inviteEmail && community) {
+      await models.AcActivity.inviteCreated({
+        email: inviteEmail,
         user_id: registeredUser ? registeredUser.id : null,
-        from_user_id: senderUser.id,
-        metadata:  { toEmail: inviteEmail, campaignName: campaignName }
-      }).then(function (inviteIn) {
-        if (inviteIn) {
-          invite = inviteIn;
-          seriesCallback();
-        } else {
-          seriesCallback('Invite not found')
-        }
-      }).catch(function (error) {
-        seriesCallback(error);
+        sender_user_id: senderUser.id,
+        community_id: communityId,
+        sender_name: senderUser.name,
+        domain_id: community.domain_id,
+        invite_id: invite.id,
+        token: token
       });
-    },
-    (seriesCallback) => {
-     if (inviteEmail && community) {
-       models.AcActivity.inviteCreated({
-         email: inviteEmail,
-         user_id: registeredUser ? registeredUser.id : null,
-         sender_user_id: senderUser.id,
-         community_id: communityId,
-         sender_name: senderUser.name,
-         domain_id: community.domain_id,
-         invite_id: invite.id,
-         token: token}, function (error) {
-         seriesCallback(error);
-       });
-     } else {
-       seriesCallback();
-     }
-    },
-    (seriesCallback) => {
-      inviteUrl = 'https://'+community.hostname+'.'+community.Domain.domain_name+'/user/accept/invite/'+token;
-      seriesCallback();
     }
-  ], (error) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback(null, inviteUrl);
-    }
-  });
+
+    inviteUrl = 'https://' + community.hostname + '.' + community.Domain.domain_name + '/user/accept/invite/' + token;
+    return inviteUrl;
+  } catch (error) {
+    throw error;
+  }
 };
 
-let counter = 0;
-let urls = [];
-async.whilst(() => { return (counter+=1)<=numberOfInvites }, (whilstCallback) => {
-  createOneInvite(masterCampaignName, null, (error, inviteUrl) => {
-    if (inviteUrl) {
-      urls.push(inviteUrl);
+const generateInvites = async () => {
+  let urls = [];
+  for (let i = 0; i < numberOfInvites; i++) {
+    try {
+      const inviteUrl = await createOneInvite(masterCampaignName, null);
+      if (inviteUrl) {
+        urls.push(inviteUrl);
+      }
+    } catch (error) {
+      console.error(error);
     }
-    whilstCallback(error);
-  })
-}, (error) => {
-  if (error)
-    console.error(error);
+  }
   urls.forEach((url) => {
     console.log(url);
   });
   process.exit();
-});
+};
+
+generateInvites();
