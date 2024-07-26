1e84ef7ffce4d842f8491cffafb786eb527825c9
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-07-26 13:57:51 +0000

    Work on Policy Synth Integration

--- server_api/ts-out/migrations/agentRegistries.cjs
+++ server_api/ts-out/migrations/agentRegistries.cjs
@@ -1,43 +1,40 @@
 'use strict';
-
 const { DataTypes } = require('sequelize');
-
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-  await queryInterface.createTable('ps_agent_registries', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    uuid: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    configuration: {
-      type: DataTypes.JSONB,
-      allowNull: false,
-    },
-  });
-
-  await queryInterface.addIndex('ps_agent_registries', ['uuid'], {
-    unique: true,
-  });
-  await queryInterface.addIndex('ps_agent_registries', ['user_id']);
-}
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('ps_agent_registries', {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            uuid: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                allowNull: false,
+            },
+            user_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            created_at: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW,
+            },
+            updated_at: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW,
+            },
+            configuration: {
+                type: DataTypes.JSONB,
+                allowNull: false,
+            },
+        });
+        await queryInterface.addIndex('ps_agent_registries', ['uuid'], {
+            unique: true,
+        });
+        await queryInterface.addIndex('ps_agent_registries', ['user_id']);
+    }
 };
