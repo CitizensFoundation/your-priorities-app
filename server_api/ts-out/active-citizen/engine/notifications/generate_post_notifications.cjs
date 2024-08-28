78dec848627fd22a47d05b03babcd6019011e1d7
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-08-28 17:34:26 +0000

    Better error handling

--- server_api/ts-out/active-citizen/engine/notifications/generate_post_notifications.cjs
+++ server_api/ts-out/active-citizen/engine/notifications/generate_post_notifications.cjs
@@ -1,18 +1,21 @@
 "use strict";
 const models = require("../../../models/index.cjs");
-const log = require('../../utils/logger.cjs');
-const async = require('async');
-const getModelAndUsersByType = require('./notifications_utils.cjs').getModelAndUsersByType;
-const addNotificationsForUsers = require('./notifications_utils.cjs').addNotificationsForUsers;
-const addOrPossiblyGroupNotification = require('./notifications_utils.cjs').addOrPossiblyGroupNotification;
+const log = require("../../utils/logger.cjs");
+const async = require("async");
+const getModelAndUsersByType = require("./notifications_utils.cjs").getModelAndUsersByType;
+const addNotificationsForUsers = require("./notifications_utils.cjs").addNotificationsForUsers;
+const addOrPossiblyGroupNotification = require("./notifications_utils.cjs").addOrPossiblyGroupNotification;
 const generateNotificationsForNewPost = (activity, callback) => {
     // Make sure not to create duplicate notifications to the same user
     const uniqueUserIds = { users: [] };
+    if (!activity) {
+        return callback("Activity is null or undefined");
+    }
     async.series([
         (seriesCallback) => {
             if (activity.Community) {
                 // Notifications for all new posts in community
-                getModelAndUsersByType(models.Community, 'CommunityUsers', activity.Community.id, "all_community", (error, community) => {
+                getModelAndUsersByType(models.Community, "CommunityUsers", activity.Community.id, "all_community", (error, community) => {
                     if (error) {
                         seriesCallback(error);
                     }
@@ -20,7 +23,10 @@ const generateNotificationsForNewPost = (activity, callback) => {
                         addNotificationsForUsers(activity, community.CommunityUsers, "notification.post.new", "all_community", uniqueUserIds, seriesCallback);
                     }
                     else {
-                        log.warn("Generate Post Notification Not found or muted", { userId: activity.user_id, type: activity.type });
+                        log.warn("Generate Post Notification Not found or muted", {
+                            userId: activity.user_id,
+                            type: activity.type,
+                        });
                         seriesCallback();
                     }
                 });
@@ -30,20 +36,28 @@ const generateNotificationsForNewPost = (activity, callback) => {
             }
         },
         (seriesCallback) => {
-            // Notifications for all new posts in group
-            getModelAndUsersByType(models.Group, 'GroupUsers', activity.Group.id, "all_group", (error, group) => {
-                if (error) {
-                    seriesCallback(error);
-                }
-                else if (group) {
-                    addNotificationsForUsers(activity, group.GroupUsers, "notification.post.new", "all_group", uniqueUserIds, seriesCallback);
-                }
-                else {
-                    log.warn("Generate Post Notification Not found or muted", { userId: activity.user_id, type: activity.type });
-                    seriesCallback();
-                }
-            });
-        }
+            if (activity.Group) {
+                // Notifications for all new posts in group
+                getModelAndUsersByType(models.Group, "GroupUsers", activity.Group.id, "all_group", (error, group) => {
+                    if (error) {
+                        seriesCallback(error);
+                    }
+                    else if (group) {
+                        addNotificationsForUsers(activity, group.GroupUsers, "notification.post.new", "all_group", uniqueUserIds, seriesCallback);
+                    }
+                    else {
+                        log.warn("Generate Post Notification Not found or muted", {
+                            userId: activity.user_id,
+                            type: activity.type,
+                        });
+                        seriesCallback();
+                    }
+                });
+            }
+            else {
+                seriesCallback();
+            }
+        },
     ], (error) => {
         callback(error);
     });
@@ -53,6 +67,9 @@ const generateNotificationsForNewPost = (activity, callback) => {
 const generateNotificationsForEndorsements = (activity, callback) => {
     // Notifications for endorsement on posts I've created
     let mutePost = false;
+    if (!activity) {
+        return callback("Activity is null or undefined");
+    }
     if (activity.Community &&
         activity.Community.configuration &&
         activity.Community.configuration.muteNotificationsForEndorsements) {
@@ -63,33 +80,39 @@ const generateNotificationsForEndorsements = (activity, callback) => {
         include: [
             {
                 model: models.User,
-                attributes: ['id', 'notifications_settings', 'email', 'name'],
+                attributes: ["id", "notifications_settings", "email", "name"],
                 required: true,
                 where: {
                     "notifications_settings.my_posts_endorsements.method": {
-                        $gt: 0
-                    }
-                }
-            }
-        ]
-    }).then((post) => {
+                        $gt: 0,
+                    },
+                },
+            },
+        ],
+    })
+        .then((post) => {
         if (post && !mutePost) {
-            addOrPossiblyGroupNotification(post, 'notification.post.endorsement', 'my_posts_endorsements', activity, post.User, 50, callback);
+            addOrPossiblyGroupNotification(post, "notification.post.endorsement", "my_posts_endorsements", activity, post.User, 50, callback);
         }
         else {
-            log.warn("Generate Post Notification Not found or muted", { userId: activity.user_id, type: activity.type });
+            log.warn("Generate Post Notification Not found or muted", {
+                userId: activity.user_id,
+                type: activity.type,
+            });
             callback();
         }
-    }).catch((error) => {
+    })
+        .catch((error) => {
         callback(error);
     });
     // TODO: Add AcWatching users
 };
 module.exports = (activity, user, callback) => {
-    if (activity.type == 'activity.post.new') {
+    if (activity.type == "activity.post.new") {
         generateNotificationsForNewPost(activity, callback);
     }
-    else if (activity.type == 'activity.post.endorsement.new' || activity.type == 'activity.post.opposition.new') {
+    else if (activity.type == "activity.post.endorsement.new" ||
+        activity.type == "activity.post.opposition.new") {
         generateNotificationsForEndorsements(activity, callback);
     }
 };
