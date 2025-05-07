686da61c37729372f0aa9cb247316ff53076012d
Robert Bjarnason <robert.bjarnason@gmail.com>
2025-05-07 21:10:50 +0000

    Work on packaging

---
+++ server_api/utils/manifest_generator.d.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+// Define the module path based on convention from other .d.ts files
+declare module '@yrpri/api/utils/manifest_generator.cjs' {
+
+  interface YpRequest extends Request {
+    ypCommunity?: YpCommunity;
+    ypDomain?: YpDomain;
+  }
+
+  /**
+   * Generates a web app manifest.
+   * The manifest content depends on the properties found on req.ypCommunity or req.ypDomain.
+   * @param req The Express request object, expected to be augmented with ypCommunity and/or ypDomain properties.
+   * @param res The Express response object used to send the generated manifest.
+   */
+  function generateManifest(req: YpRequest, res: Response): void;
+
+  export default generateManifest;
+}
