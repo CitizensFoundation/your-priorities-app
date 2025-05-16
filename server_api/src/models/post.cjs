8d7675d1e74f428b0c3d943aab1e5a56943866a0
Robert Bjarnason <robert.bjarnason@gmail.com>
2025-05-16 19:28:34 +0000

    Work on agent and make env var required

--- server_api/src/models/post.cjs
+++ server_api/src/models/post.cjs
@@ -217,6 +217,7 @@ module.exports = (sequelize, DataTypes) => {
   Post.CONTENT_BLOG = 4;
   Post.CONTENT_QUESTION = 5;
   Post.CONTENT_SURVEY = 6;
+  Post.AGENT_CONVERSATION = 7;
 
   Post.getSearchVector = () => {
     return 'PostText';
